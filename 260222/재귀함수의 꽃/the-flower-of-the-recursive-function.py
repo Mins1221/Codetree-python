77028fc9dc3075f2c565504d56b6bf6242feb22c
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-02-22 15:29:01 +0900

    Title: 재귀함수의 꽃, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260222/재귀함수의 꽃/the-flower-of-the-recursive-function.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+# Please write your code here.
+def print_number(n):
+    if n ==0:
+        return
+
+    print(n,end =' ')
+    print_number(n-1)
+    print(n,end =' ')
+
+print_number(n)
