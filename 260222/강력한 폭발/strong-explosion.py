7630490e7fbc814beefe9d56fe5260e743f27d5d
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-02-22 19:42:27 +0900

    Title: 강력한 폭발, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 260222/강력한 폭발/strong-explosion.py
+++ 260222/강력한 폭발/strong-explosion.py
@@ -8,33 +8,64 @@ for i in range(n):
 bombs = []
 for r in range(n):
     for c in range(n):
-        if board[r][c] == 1
+        if board[r][c] == 1:
             bombs.append((r,c))
-max_count =0
+
+
+
+
 
 def find(idx):
     if idx == len(bombs):
-        return max_count
+        global max_count
+        current = 0
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] == 1:
+                    current += 1
+        max_count = max(max_count, current)
+        return 
+    r, c = bombs[idx]
+    
+    for i in range(1, 4):
+        changed = []
+        if i == 1:
+            bomb1(r, c, changed)
+        elif i == 2:
+            bomb2(r, c, changed)
+        elif i == 3:
+            bomb3(r, c, changed)
+        
+        find(idx + 1)
+        
+        # 되돌리기
+        for r2, c2 in changed:
+            board[r2][c2] = 0
 
-    폭탄1 (상하 2칸)
-    (r-2,c) (r-1,c) (r,c) (r+1,c) (r+2,c)
-def bomb1(r,c):
-    board[r-2][c] =1
-    board[r-1][c] =1
-    board[r][c] =1
-    board[r+1][c] =1
-    board[r+2][c] =1
+def bomb1(r, c, changed):
+    for nr in [r-2, r-1, r, r+1, r+2]:
+        if 0 <= nr < n:        # 범위 체크
+            if board[nr][c] == 0:  # 원래 0인 칸만
+                board[nr][c] = 1
+                changed.append((nr, c))  # 기록
 
-def bomb2(r,c):
-    board[r-2][c] =1
-    board[r-2][c] =1
-    board[r-2][c] =1
-    (r,c) (r-1,c) (r+1,c) (r,c-1) (r,c+1)
+def bomb2(r, c, changed):
+    for nr, nc in [(r,c),(r-1,c),(r+1,c),(r,c-1),(r,c+1)]:
+        if 0 <= nr < n and 0 <= nc < n:
+            if board[nr][nc] == 0:
+                board[nr][nc] = 1
+                changed.append((nr, nc))
 
-폭탄3 (대각선 1칸)
-    (r,c) (r-1,c-1) (r-1,c+1) (r+1,c-1) (r+1,c+1)
+
+def bomb3(r, c, changed):
+    for nr, nc in [(r,c),(r-1,c-1),(r-1,c+1),(r+1,c-1),(r+1,c+1)]:
+        if 0 <= nr < n and 0 <= nc < n:
+            if board[nr][nc] == 0:
+                board[nr][nc] = 1
+                changed.append((nr, nc))
+
 
     
-
+max_count =0
 find(0)
 print(max_count)
