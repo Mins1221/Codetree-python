9857334674d70f8087a8f9c7a281852f19ecce13
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-03-04 17:39:51 +0900

    Title: 수들의 합 최대화하기, Time: 85ms, Memory: 19MB, Status: Passed - Codetree

--- 260304/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 260304/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -2,22 +2,22 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-visited = [False] * n
-max_val =0
+visited = [False] *n
+max_val = 0
 
-def max_num(col, curr_sum):
+def solve(col,curr_sum):
     global max_val
-    if col == n:
+    if col == n :
         max_val = max(max_val,curr_sum)
         return
 
     for row in range(n):
-        if visited[row]:
+        if visited[row] : 
             continue
-
+        
         visited[row] = True
-        max_num(col+1,curr_sum + grid[row][col])
+        solve(col+1, curr_sum + grid[row][col])
         visited[row] = False
-max_num(0,0)
+
+solve(0,0)
 print(max_val)
-            
