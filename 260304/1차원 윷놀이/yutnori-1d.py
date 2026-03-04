ae15c47f1c74b9fcc181c685568f195fed8640ca
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-03-04 18:54:19 +0900

    Title: 1차원 윷놀이, Time: 2807ms, Memory: 25MB, Status: Passed - Codetree

--- 260304/1차원 윷놀이/yutnori-1d.py
+++ 260304/1차원 윷놀이/yutnori-1d.py
@@ -1,28 +1,27 @@
-n, m, k = map(int, input().split())
+n, m, k = tuple(map(int, input().split()))
 nums = list(map(int, input().split()))
-
+pieces = [1 for _ in range(k)]
+ans =0
 # Please write your code here.
-def choose(curr_turn,horses,score):
-    if curr_turn == n:   
-        return score
+def calc():
+    score = 0
+    for piece in pieces :
+        score += (piece >= m)
+    return score
 
+def find_max(cnt):
+    global ans
 
-    max_score = 0
+    ans = max(ans,calc())
 
+    if cnt == n:
+        return
     for i in range(k):
-        new_horses = horses[:]
-        new_horses[i] += nums[curr_turn]
-        # M까지 도달했는지 확인
-        new_score = score
-        if new_horses[i] >= m and horses[i] <m:
-            new_score += 1
-        max_score = max(max_score,choose(curr_turn+1,new_horses,new_score))      
+        if pieces[i] >= m:
+            continue
+        pieces[i] += nums[cnt]
+        find_max(cnt +1)
+        pieces[i] -= nums[cnt]
 
-    return max_score 
-
-   
-horses = [1] * k  # 전역에서 초기화 후 호출
-choose(0, horses, 0)
-
-print(choose(0, horses, 0))
-        
+find_max(0)
+print(ans)
