9be935cf16411c18c645c1596db1a930fa3f8198
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-03-07 18:58:56 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 66ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 260307/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,41 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]  # 이 줄 추가!
+order = 0
+answer = [[0] * m for _ in range(n)]   # answer도 선언 필요!
+cnt = 0
+# Please write your code here.
+def dfs(x,y):
+    global order
+    global cnt
+    dxs,dys =[1,0],[0,1]
+
+    answer[x][y] = order
+    order +=1
+    visited[x][y] = 1
+
+    if x == n-1 and y == m-1:  # 목적지 도달 시
+        cnt += 1
+        visited[x][y] = 0      # 복원
+        return
+
+    for dx,dy in zip(dxs,dys):
+        new_x, new_y = x+dx, y+ dy
+
+        if can_go(new_x,new_y):
+            dfs(new_x,new_y)
+
+def in_range(x,y):
+    return 0 <= x and x<n and\
+            0 <= y and y<m
+
+def can_go(x,y):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] or grid[x][y] ==0:
+        return False
+    return True
+
+
+dfs(0,0)
+print(cnt)
