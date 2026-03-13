08ef1aa11f305ec92b33166f6b8eb61a97cd2e8c
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-03-13 16:59:16 +0900

    Title: 피보나치 수, Time: 80ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260313/피보나치 수/fibonacci-number.py
+++ 260313/피보나치 수/fibonacci-number.py
@@ -1,9 +1,8 @@
 N = int(input())
-dp = [0] * (N+1)
-if N >= 1:
-    dp[1] = 1
-if N >= 2:
-    dp[2] = 1
+MAX_NUM = 45
+dp = [0] * MAX_NUM
+dp[1] =1
+dp[2] =1
 # Please write your code here.
 for i in range(3, N+1):
     dp[i] = dp[i-1] + dp[i-2]
