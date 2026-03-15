5037461fb6da04e722203c052282d419d7650d9a
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-03-15 15:59:27 +0900

    Title: 사각형 채우기 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260315/사각형 채우기 2/rectangle-fill-2.py
@@ -0,0 +1,14 @@
+n = int(input())
+max_num = 1000
+mod = 10007
+# Please write your code here.
+dp = [0] * max_num
+dp[1] = 1 
+dp[2] = 3
+dp[3] = 5 
+
+for i in range(4, (n+1)):
+    dp[i] =  dp[i-1] + 2* dp[i-2] 
+
+print(dp[n])
+
