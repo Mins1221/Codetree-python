455ead3c9a92ad85e45b3a8a04950fc26f90f693
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-03-17 14:15:30 +0900

    Title: 모이자, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260317/모이자/gather.py
@@ -0,0 +1,13 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+min_sum = 10000
+for i in range(n):
+    sum_diff = 0
+    for j in range(n):
+        sum_diff += abs(j-i) *A[j]
+
+    min_sum = min(min_sum,sum_diff)
+
+print(min_sum)
