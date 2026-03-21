eda53faa644a9c8535b2ebf644c012b105eaf37e
Mins1221 <127467371+Mins1221@users.noreply.github.com>
2026-03-21 21:43:28 +0900

    Title: 계단 오르기 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260321/계단 오르기 2/climbing-stairs-2.py
+++ 260321/계단 오르기 2/climbing-stairs-2.py
@@ -2,22 +2,25 @@ import sys
 n = int(input())
 arr = [0] + list(map(int, input().split()))
 INT_MIN = -sys.maxsize
-dp = [[0] * 4 for _ in range(n+1)]
+dp = [[0]* 4 for _ in range(n+1)]
 def initialize():
     for i in range(n+1):
         for j in range(4):
             dp[i][j] = INT_MIN
+
     dp[0][0] = 0
+
 initialize()
 
 for i in range(1,n+1):
     for j in range(0,4):
-        if dp[i-2][j] != INT_MIN:
-            dp[i][j] = max(dp[i][j], dp[i-2][j] + arr[i])
-        if j >= 1 and dp[i-1][j-1] != INT_MIN:
-            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + arr[i])
+        if j == 0 :
+            dp[i][j] = dp[i-2][j] + arr[i]
+        else:
+            dp[i][j] = max(dp[i-2][j] + arr[i],dp[i-1][j-1]+arr[i])
 
 ans = INT_MIN
 for i in range(1,n+1):
     ans = max(dp[n])
+
 print(ans)
