9e87ec30c6690ac7d4ca5032275679492a046c0f
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-04 16:38:48 +0900

    Title: 편집거리 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/편집거리 3/edit-distance-3.py
+++ 260404/편집거리 3/edit-distance-3.py
@@ -8,11 +8,9 @@ dp = [[0 for _ in range(len(B)+1)] for _ in range(len(A)+1)
 def initialize():
     dp[0][0] = 0 
 
-    for i in range(1,len(A)+1):
-        
+    for i in range(1,len(A)+1): 
             dp[i][0] = dp[i-1][0] +1
-    for j in range(1,len(B)+1):
-        
+    for j in range(1,len(B)+1):    
             dp[0][j] = dp[0][j-1] +1
 
 initialize()
