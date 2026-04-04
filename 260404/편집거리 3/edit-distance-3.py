f9c208026690691e9bc72f9ce8fbf9cc1e7a2a96
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-04 16:38:38 +0900

    Title: 편집거리 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/편집거리 3/edit-distance-3.py
+++ 260404/편집거리 3/edit-distance-3.py
@@ -6,26 +6,22 @@ dp = [[0 for _ in range(len(B)+1)] for _ in range(len(A)+1)
         ]
 
 def initialize():
-    dp[1][1] = 1 if A[1] == B[1] else 2
+    dp[0][0] = 0 
 
-    for i in range(2,len(A)+1):
-        if A[i-1] == B[0] :
-            dp[i][1] = i
-        else:
-            dp[i][1] = dp[i-1][1] +1
-    for j in range(2,len(B)+1):
-        if A[0] == B[j-1]:
-            dp[1][j] = j
-        else:
-            dp[1][j] = dp[1][j-1] +1
+    for i in range(1,len(A)+1):
+        
+            dp[i][0] = dp[i-1][0] +1
+    for j in range(1,len(B)+1):
+        
+            dp[0][j] = dp[0][j-1] +1
 
 initialize()
 
-for i in range(2,len(A)+1):
-    for j in range(2,len(B)+1):
+for i in range(1,len(A)+1):
+    for j in range(1,len(B)+1):
         if A[i-1] == B[j-1] :
-            dp[i][j] = dp[i-1][j-1] + 1
+            dp[i][j] = dp[i-1][j-1]
         else:
             dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + 1
 
-print(dp[len(A)][len(B)] //2)
+print(dp[len(A)][len(B)])
