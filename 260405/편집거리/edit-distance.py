2e70996c041d8acfa31a20cf3cef043ec5808e60
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-05 14:45:53 +0900

    Title: 편집거리, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260405/편집거리/edit-distance.py
@@ -0,0 +1,25 @@
+A = input()
+B = input()
+
+# Please write your code here.
+dp = [[0 for _ in range(len(B)+1)] for _ in range(len(A)+1)
+        ]
+
+def initialize():
+    dp[0][0] = 0 
+
+    for i in range(1,len(A)+1): 
+            dp[i][0] = dp[i-1][0] +1
+    for j in range(1,len(B)+1):    
+            dp[0][j] = dp[0][j-1] +1
+
+initialize()
+
+for i in range(1,len(A)+1):
+    for j in range(1,len(B)+1):
+        if A[i-1] == B[j-1] :
+            dp[i][j] = dp[i-1][j-1]
+
+        else : 
+            dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]) + 1
+print(dp[len(A)][len(B)])
