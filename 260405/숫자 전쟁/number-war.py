5900eb3001dbc7971bf55297c6f2747db8a86987
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-05 15:43:17 +0900

    Title: 숫자 전쟁, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/숫자 전쟁/number-war.py
+++ 260405/숫자 전쟁/number-war.py
@@ -13,10 +13,6 @@ for i in range(n-1, -1, -1):
         elif A[i] < B[j] : 
             dp[i][j] = max(dp[i+1][j+1], dp[i+1][j])
         else :
-            dp[i][j] = max(dp[i+1][j+1],dp[i][j])
+            dp[i][j] = dp[i+1][j+1]
 
-ans = 0
-for i in range(n):
-    ans = max(ans,dp[i][j])
-
-print(ans)
+print(dp[0][0])
