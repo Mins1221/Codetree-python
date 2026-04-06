fe8426f35403c3dc5540788e6a296ce324d4817c
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-06 12:50:25 +0900

    Title: 신전 탐험하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/신전 탐험하기 2/explore-temple-2.py
+++ 260406/신전 탐험하기 2/explore-temple-2.py
@@ -5,22 +5,22 @@ a = [[0 for _ in range(4)] for _ in range(n+1)]
 for i in range(1,n+1):
     a[i][1],a[i][2],a[i][3] = map(int,input().split())
 
-INF = sys.maxsize
-ans = 0
+dp = [[[0 for _ in range(4)]for _ in range(4)] for _ in range(n+1)]
+for j in range(1,4):
+    dp[1][j][j] = a[1][j]
 
-for start in (1,2,3):
-    dp = [[-INF] * 4 for _ in range(n+1)]
-    dp[1][start] = a[1][start]
-    for i in range(1,n):
-        for j in range(1,4):
-            for k in range(1,4):
-                if j != k:
-                    dp[i+1][k] = max(dp[i+1][k], dp[i][j] + a[i+1][k]) 
-
-    for i in range(1,4):
-        if i != start:
-            ans = max(ans,dp[n][i])
-
-
-
+for i in range(1,n):
+    for j in range(1,4):
+        for k in range(1,4):
+            for l in range(1,4):
+                if k == l:
+                    continue
+                dp[i+1][j][l] = max(dp[i+1][j][l], dp[i][j][k]+ a[i+1][l])
+        
+ans =0
+for j in range(1,4):
+    for k in range(1,4):
+        if j ==k:
+            continue
+        ans = max(ans,dp[n][j][k])
 print(ans)
