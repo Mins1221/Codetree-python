6f4a33c6af4262d4fb168acdab09e198c4e51811
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-06 14:29:01 +0900

    Title: 신전 탐험하기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/신전 탐험하기 3/explore-temple-3.py
+++ 260406/신전 탐험하기 3/explore-temple-3.py
@@ -1,15 +1,16 @@
 n, m = map(int, input().split())
-a = [[0]]  # 더미
-a += [[0] + list(map(int, input().split())) for _ in range(n)]
+a = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-dp = [[0 for _ in range(m+1)] for _ in range(n+2)]
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
 
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        for k in range(1,m+1):
+for i in range(n):
+    for j in range(m):
+        for k in range(m):
             if j != k :
                 dp[i+1][k] = max(dp[i + 1][k] , dp[i][j] + a[i][k])
-ans = max(dp[n+1][1:m+1])
+
+ans = max(dp[n])
 
 print(ans)
+
