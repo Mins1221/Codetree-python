8db29b1e3aeb9d22dea1f80e4850d9d334bfdfe6
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-06 12:37:20 +0900

    Title: 신전 탐험하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/신전 탐험하기 2/explore-temple-2.py
+++ 260406/신전 탐험하기 2/explore-temple-2.py
@@ -1,17 +1,26 @@
+import sys
+
 n = int(input())
 a = [[0 for _ in range(4)] for _ in range(n+1)]
 for i in range(1,n+1):
     a[i][1],a[i][2],a[i][3] = map(int,input().split())
 
-dp = [[0 for _ in range(4)] for _ in range(n+1)]
+INF = sys.maxsize
+ans = 0
 
-for i in range(n):
-    for j in range(4):
-        for k in range(4):
+for start in (1,2,3):
+    dp = [[-INF] * 4 for _ in range(n+1)]
+    dp[1][start] = a[1][start]
+    for i in range(1,n):
+        for j in range(1,4):
+            for k in range(1,4):
                 if j != k:
                     dp[i+1][k] = max(dp[i+1][k], dp[i][j] + a[i+1][k]) 
 
+    for i in range(1,4):
+        if i != start:
+            ans = max(ans,dp[n][i])
 
-if dp[1][j] != dp[n][j]:
-    ans = max(dp[n])
+
+
 print(ans)
