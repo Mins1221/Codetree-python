4955e4568dd4ee6a771265b57f4e98cb494e60da
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-09 16:55:04 +0900

    Title: 수 등장 횟수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/수 등장 횟수/number-frequency.py
+++ 260409/수 등장 횟수/number-frequency.py
@@ -3,15 +3,16 @@ arr = list(map(int, input().split()))
 nums = list(map(int, input().split()))
 
 # Please write your code here.
-num_index = {}
-for i, elem in enumerate(arr):
-    num_index[i+1] = elem  
+cnt = {}
 
-count_map = {}
-for v in num_index.values():
-    if v in count_map:
-        count_map[v] += 1
+for elem in arr:
+    if elem not in cnt:
+        cnt[elem] = 1
     else:
-        count_map[v] = 1
+        cnt[elem] +=1
 
-print(' '.join(str(count_map.get(x, 0)) for x in nums))
+for num in nums:
+    if num not in cnt:
+        print(0, end=" ")
+    else:
+        print(cnt[num], end = " ")
