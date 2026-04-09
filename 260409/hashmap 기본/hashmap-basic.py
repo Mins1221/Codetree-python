662f8e7842ede3b9671f409b1a1de4f89e574183
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-09 15:30:52 +0900

    Title: hashmap 기본, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260409/hashmap 기본/hashmap-basic.py
@@ -0,0 +1,25 @@
+n = int(input())
+commands = []
+line = []
+d =dict()
+for _ in range(n):
+    line = input().split()
+    cmd = line[0]
+    k = int(line[1])
+    if cmd == "add":
+        v = int(line[2])
+        commands.append((cmd, k, v))
+        d[k]=v
+    else:
+        commands.append((cmd, k,v))
+    if cmd == "remove":
+        d.pop(k)
+    if cmd == "find":
+        if k in d:
+            print(d[k])
+        else:
+            print("None")
+# Please write your code
+
+
+    
