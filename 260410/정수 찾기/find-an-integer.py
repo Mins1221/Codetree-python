af92f1533c37c6d3dca8b6db71cfdcab751d6471
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-10 15:35:50 +0900

    Title: 정수 찾기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/정수 찾기/find-an-integer.py
+++ 260410/정수 찾기/find-an-integer.py
@@ -1,14 +1,16 @@
+import sys
 n = int(input())
-a = list(map(int, input().split()))
+
+a = list(map(int, sys.stdin.readline().split()))  # 대용량 입력에 더 안정적)
 
 m = int(input())
-b = list(map(int, input().split()))
+b = list(map(int, sys.stdin.readline().split()))
 
 # Please write your code here.
 s1 = set(a)
 s2 = set(b)
 
-for eleml in s2:
+for eleml in b:
     if eleml in s1:
         print(1)
     else:
