fdded35130b74c58fb01df9040d22048223f1156
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-10 16:37:04 +0900

    Title: 정수 명령어, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/정수 명령어/Integer-command.py
+++ 260410/정수 명령어/Integer-command.py
@@ -1,26 +1,25 @@
 from sortedcontainers import SortedSet
-
 T = int(input())
 
-for _ in range(T):          # T번 반복
-    s = SortedSet()         # 테스트케이스마다 초기화
+for _ in range(T):
+    s = SortedSet()
     k = int(input())
     operations = [tuple(input().split()) for _ in range(k)]
-
-    for op in operations:   # 각 명령어 바로 실행
+    for op in operations:
         cmd = op[0]
         n = int(op[1])
-
-        if cmd == "I":
+        if cmd =="I":
             s.add(n)
-        elif cmd == "D":
-            if s:           # 비어있으면 무시
+        if cmd =="D":
+            if s:
                 if n == 1:
-                    s.remove(s[-1])  # 최댓값 삭제
+                    s.remove(s[-1])
                 else:
-                    s.remove(s[0])   # 최솟값 삭제
-
+                    s.remove(s[0])
     if s:
-        print(s[-1], s[0])  # 최댓값 최솟값
+        print(s[-1],s[0])
     else:
         print("EMPTY")
+        
+    # Please write your code here.
+        
