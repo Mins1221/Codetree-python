9a77bab4734d235c1e00d6a61e8f1ce2da340ded
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-13 13:59:11 +0900

    Title: top K 숫자, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260413/top K 숫자/top-k-elements.py
@@ -0,0 +1,10 @@
+from sortedcontainers import SortedSet
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+s = SortedSet()
+# Please write your code here.
+for i in arr:
+    s.add(i)
+    new = sorted(s,reverse= True)
+for j in range(min(k, len(new))):
+    print(new[j], end=" ")
