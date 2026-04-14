9b905e8e55afc95870e4501311a15fb2d8fbfd8d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-14 18:35:51 +0900

    Title: 초대장과 번호표, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260414/초대장과 번호표/invitation-and-number-tag.py
@@ -0,0 +1,25 @@
+N, G = map(int, input().split())
+
+group = []
+group_size = []
+groups = set()
+for _ in range(G):
+    nums = list(map(int, input().split()))
+    group_size.append(nums[0])
+    group.append(nums[1:])
+
+invited = set([1])
+
+while True:
+    changed = False
+    for i in group:
+        notInvited = set(i) - invited
+        if len(notInvited) == 1:
+            invited.add(next(iter(notInvited)))
+            changed = True
+        
+    if not changed :
+        break
+
+    
+    print(len(invited))
