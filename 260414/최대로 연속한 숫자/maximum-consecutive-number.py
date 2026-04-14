c87c5864fa2c0eddfdb6721ba98941be4c0fad59
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-14 19:54:12 +0900

    Title: 최대로 연속한 숫자, Time: 0ms, Memory: 0MB, Status: Memory Limit Exceed - Codetree

--- 260414/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 260414/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -1,20 +1,29 @@
-from sortedcontainers import SortedSet
+from sortedcontainers import SortedSet,SortedList
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 s = SortedSet()
 # Please write your code here.
 for l in range(n+1):
     s.add(l)
-
+runs = SortedList([n+1])
  
 for k in nums:
+    idx = s.index(k)
     s.remove(k)
-    num = 1
-    max_num =1
-    for i in range(len(s)-1):
-        if s[i+1] -s[i] ==1:
-            num +=1
-            max_num = max(max_num,num)
-        else:
-            num =1
-    print(max_num)
+    left_idx = idx - 1
+    left_count = 0
+    while left_idx >= 0 and s[left_idx] == k - 1 - left_count:
+        left_count += 1
+        left_idx -= 1
+
+    right_idx = idx 
+    right_count = 0
+    while right_idx < len(s) and s[right_idx] == k+1 +right_count: 
+        right_count += 1
+        right_idx += 1 # 초기엔 전체가 연속
+
+    runs.remove(left_count + right_count + 1)
+    if left_count > 0: runs.add(left_count)
+    if right_count > 0: runs.add(right_count)
+    
+    print(runs[-1])
