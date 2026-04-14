3f2de2472edc7cfcde54321f17eefefdbea16c1f
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-14 12:34:27 +0900

    Title: 원소의 합이 0, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260414/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,22 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
+d1 = {}
+d2 = {}
+# Please write your code here.'
+ans = 0
+for i in A:
+    for j in B:
+        sum1 = i + j
+        d1[sum1] = d1.get(sum1, 0) + 1
+
+for k in C:
+    for l in D:
+        sum2 = -(k + l)
+        if sum2 in d1:
+            d1.get(sum2,0) + 1
+            ans +=1
+
+print(ans)
