f375d92700ad38324557328efadb6596c0105349
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-24 15:49:21 +0900

    Title: 구간에 속한 문자의 개수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 260424/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -1,31 +1,37 @@
-def solve():
-    N, M, K = map(int, input().split())
-    grid = []
-    for _ in range(N):
-        grid.append(input().strip())
+MAX_C = 3
 
-    # a, b, c 각각 2D prefix sum
-    prefix = {ch: [[0]*(M+1) for _ in range(N+1)] for ch in 'abc'}
+n,m,k = tuple(map(int,input().split()))
+arr =  [[0]*(m+1) for _ in range(n+1)]
+prefix_sum = [[[0]*(m+1) for _ in range(n+1)] for _ in range(MAX_C +1)]
 
-    for i in range(1, N+1):
-        for j in range(1, M+1):
-            for ch in 'abc':
-                val = 1 if grid[i-1][j-1] == ch else 0
-                prefix[ch][i][j] = (prefix[ch][i-1][j]
-                                  + prefix[ch][i][j-1]
-                                  - prefix[ch][i-1][j-1]
-                                  + val)
+def get_sum(c,x1,y1,x2,y2):
+    return prefix_sum[c][x2][y2] - prefix_sum[c][x1-1][y2] -\
+           prefix_sum[c][x2][y1-1] + prefix_sum[c][x1-1][y1-1]
+        
+for i in range(1,n+1):
+    row = input()
+    for j in range(1,m+1):
+            if row[j-1] == 'a':
+                arr[i][j] =1 
+            elif row[j-1] == 'b':
+                arr[i][j] =2
+            else:
+                arr[i][j] =3
 
-    def query(ch, r1, c1, r2, c2):
-        return (prefix[ch][r2][c2]
-              - prefix[ch][r1-1][c2]
-              - prefix[ch][r2][c1-1]
-              + prefix[ch][r1-1][c1-1])
+for c in range(1,4):
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            additional_value = 0
+            if arr[i][j] == c:
+                additional_value =1
+            
+            prefix_sum[c][i][j] = prefix_sum[c][i-1][j] +\
+                                  prefix_sum[c][i][j-1] -\
+                                  prefix_sum[c][i-1][j-1] + \
+                                  additional_value
 
-    for _ in range(K):
-        r1, c1, r2, c2 = map(int, input().split())
-        print(query('a',r1,c1,r2,c2),
-              query('b',r1,c1,r2,c2),
-              query('c',r1,c1,r2,c2))
-
-solve()
+for _ in range(k):
+    x1,y1,x2,y2 = tuple(map(int,input().split()))
+    print(get_sum(1,x1,y1,x2,y2),
+    get_sum(2,x1,y1,x2,y2),
+    get_sum(3,x1,y1,x2,y2))
