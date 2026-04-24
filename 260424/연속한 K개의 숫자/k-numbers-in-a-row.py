a0d8a0f05f0b9ba870a80294493efc53bf8462bc
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-24 22:58:49 +0900

    Title: 연속한 K개의 숫자, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260424/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 260424/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,25 +1,16 @@
 import sys
 INT_MAX = sys.maxsize
-n, k, b = tuple(map(int, input().split()))
+n,k,b = tuple(map(int,input().split()))
 arr = [0] *(n+1)
-prefix_sum = [0] *(n+1)
-
-def get_sum(s,e):
-    return prefix_sum[e] - prefix_sum[s-1]
-
-ans = INT_MAX
-
-for _ in range(b):
+prefix_sum = [0] * (n+1)
+for i in range(b):
     x = int(input())
-
     arr[x] = 1
 
-prefix_sum[0] = 0
-
-for i in range(1,n+1):
+for i in range(n):
     prefix_sum[i] = prefix_sum[i-1] + arr[i]
-
-for i in range(1,n-k +2):
-    ans = min(get_sum(i,i+k-1),ans)
+ans = INT_MAX
+for i in range(1,n-k+2):
+    ans = min(ans,prefix_sum[n-k+1]-prefix_sum[i-1])
 
 print(ans)
