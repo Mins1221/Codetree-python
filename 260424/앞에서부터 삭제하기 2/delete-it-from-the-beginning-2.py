856b0c06b44b6c84f18bcf44bd2af2f1f47482a1
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-24 14:33:25 +0900

    Title: 앞에서부터 삭제하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 260424/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,25 +1,21 @@
 import heapq
-
 n = int(input())
 arr = list(map(int, input().split()))
 
-pq = arr[1:][:] 
-heapq.heapify(pq)
-total = sum(arr[1:])
-to_remove = {}
-max_avg = float('-inf')
+sum_val =0
+max_val =0
+pq = []
 
-for k in range(1, n-1):
-    # 삭제 예약된 원소가 top에 있으면 제거
-    while to_remove.get(pq[0], 0) > 0:
-        val = heapq.heappop(pq)
-        to_remove[val] -= 1
+heapq.heappush(pq,arr[n-1])
+sum_val += arr[n-1]
+for i in range(n-2,0,-1):
+    heapq.heappush(pq,arr[i])
+    sum_val +=arr[i]
 
-    avg = (total - pq[0]) / (n - k - 1)
-    max_avg = max(max_avg, avg)
+    min_num = pq[0]
+    avg = (sum_val - min_num)/ (n-i-1)
 
-    # 다음 k로 이동: arr[k]를 삭제 예약
-    total -= arr[k]
-    to_remove[arr[k]] = to_remove.get(arr[k], 0) + 1
+    if max_val < avg :
+        max_val = avg
 
-print(f"{max_avg:.2f}")
+print(f"{max_val:.2f}")
