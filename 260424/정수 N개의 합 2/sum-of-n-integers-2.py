815f740e58e7572fe60ca4f2f1cf359e2ebbb157
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-24 22:19:35 +0900

    Title: 정수 N개의 합 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260424/정수 N개의 합 2/sum-of-n-integers-2.py
+++ 260424/정수 N개의 합 2/sum-of-n-integers-2.py
@@ -1,16 +1,13 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
-prefix_sum = [0] * (n+1)
-
-
+prefix_sum = [0] *n
+# Please write your code here.
 prefix_sum[0] = arr[0]
-for i in range(1,n):
+for i in range(n):
     prefix_sum[i] = prefix_sum[i-1] + arr[i]
 
-max_sum = 0
-for i in range(len(arr) - k + 1):
-    current = prefix_sum[i+k] - prefix_sum[i]
-    if current > max_sum:
-        max_sum = current
+max_val = 0
+for i in range(n):
+    max_val =max(max_val,prefix_sum[i] - prefix_sum[i-k])
 
-print(max_sum) 
+print(max_val)
