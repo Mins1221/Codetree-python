0a7801b2be6eca00a2c2460c59d6fbacae28c2d7
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-25 01:00:58 +0900

    Title: 두 수의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/두 수의 합/sum-of-two-num.py
+++ 260425/두 수의 합/sum-of-two-num.py
@@ -2,9 +2,12 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 count = 0
 d = dict()
-for i,elem in enumerate(arr):
+for elem in arr:
     if k-elem in d:
-        count +=1
-    d[elem] = i+1
+        count += d[k - elem]
+    if elem in d:
+        d[elem] +=1
+    else:
+        d[elem] =1
     
 print(count)
