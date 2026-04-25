3abe2f05ec687642e0b7d1ac9be06c616d5db63a
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-25 16:57:29 +0900

    Title: 최대 감소 부분 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260425/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [0] * (n+1)
+
+for i in range(1,n):
+    for j in range(i):
+        if a[j] > a[i] :
+            dp[i] = max(dp[i],dp[j]+1)
+
+print(max(dp)+1)
