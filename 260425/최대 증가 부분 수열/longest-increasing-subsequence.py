59c3baad46cf91a070fbf6ec6a2b9e9718528fe0
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-25 16:53:32 +0900

    Title: 최대 증가 부분 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260425/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = [0] + list(map(int, input().split()))
+
+dp = [0] * (n+1)
+
+for i in range(1,n+1):
+    for j in range(i):
+        if a[j] < a[i] :
+            dp[i] = max(dp[j]+1, dp[i])
+
+print(max(dp))
