ac5abf00e431027e82a784a3431d09a7cdf47f1a
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-25 18:31:59 +0900

    Title: 최대 동전 거슬러주기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260425/최대 동전 거슬러주기/max-coin-change.py
+++ 260425/최대 동전 거슬러주기/max-coin-change.py
@@ -1,13 +1,21 @@
-n, m = map(int, input().split())  # n=목표금액, m=동전종류수
-coins = list(map(int, input().split()))
+import sys
+n,m= map(int, input().split())
+coin = [0] + list(map(int, input().split()))
+INT_MIN = -sys.maxsize
+dp = [0] * (m+1)
+for i in range(m+1):
+    dp[i] = INT_MIN
 
-dp = [-1] * (m + 1)  # -1 = 만들 수 없음
 dp[0] = 0
+for i in range(1,m+1):
+    for j in range(1,n+1):
+        if dp[i-coin[j]] == INT_MIN:
+            continue
+        if i >= coin[j] :
+            dp[i] = max(dp[i], dp[i-coin[j]]+1)
+        
+ans = dp[m]
+if ans == INT_MIN:
+    print(-1)
 
-for i in range(1, m + 1):      # i = 금액
-    for coin in coins:          # 모든 동전 시도
-        if i >= coin and dp[i - coin] != -1:
-            dp[i] = max(dp[i], dp[i - coin] + 1)
-
-print(dp[m]) 
-
+print(ans)
