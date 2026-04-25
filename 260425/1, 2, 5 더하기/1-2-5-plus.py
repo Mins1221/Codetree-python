4cfb8800bc7d3508e56d091568d53d2a7537b040
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-25 17:44:14 +0900

    Title: 1, 2, 5 더하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260425/1, 2, 5 더하기/1-2-5-plus.py
+++ 260425/1, 2, 5 더하기/1-2-5-plus.py
@@ -1,11 +1,10 @@
 n = int(input())
+dp = [0] * (n + 1)
+dp[0] = 1  # 핵심: 빈 합 = 1가지
 
-dp = [0] * (n+1)
-dp[1] = 1
-dp[2] = 1
-dp[3] = 3
-dp[4] = 5
-dp[5] = 9
-for i in range(5,n+1):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-5]
-print(dp[n]+1)
+for i in range(1, n + 1):
+    dp[i] = dp[i-1]
+    if i >= 2: dp[i] += dp[i-2]
+    if i >= 5: dp[i] += dp[i-5]
+
+print(dp[n])
