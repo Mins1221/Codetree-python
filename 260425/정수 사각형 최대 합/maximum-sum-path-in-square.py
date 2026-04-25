e97cbcef7750c5ebcbe232ebad8acacfad280922
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-25 15:45:13 +0900

    Title: 정수 사각형 최대 합, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260425/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 260425/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -3,12 +3,14 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [[0 for _ in range(n)] for _ in range(n)]
 
-dp[0][0] = grid[0][0]
+
 
 for i in range(n):
+    dp[0][0] = grid[0][0]
     dp[i][0] = dp[i-1][0] + grid[i][0]
 
 for j in range(n):
+    dp[0][0] = grid[0][0]
     dp[0][j] = dp[0][j-1] + grid[0][j]
 
 for i in range(1,n):
