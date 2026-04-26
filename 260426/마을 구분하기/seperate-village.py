1b449696174f7c7ead4af9aebbf6ce9ca2d9b939
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-04-26 18:50:22 +0900

    Title: 마을 구분하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260426/마을 구분하기/seperate-village.py
@@ -0,0 +1,47 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[False for _ in range(n)] for _ in range(n)]
+
+people_num = 0
+people_nums = list()
+
+def in_range(x,y):
+    return 0 <= x < n and 0 <= y < n
+
+def can_go(x,y):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] or grid[x][y] == 0:
+        return False
+    return True
+
+def dfs(x,y):
+    global people_num
+    dxs, dys = [0,1,0,-1] , [1,0,-1,0]
+
+    for dx,dy in zip(dxs,dys):
+        new_x , new_y = x+dx , y+dy
+
+        if can_go(new_x,new_y):
+            visited[new_x][new_y] = True
+            people_num +=1
+            dfs(new_x,new_y)
+
+for i in range(n):
+    for j in range(n):
+        if can_go(i,j):
+            visited[i][j] = True
+            people_num = 1
+
+            dfs(i,j)
+
+            people_nums.append(people_num)
+
+people_nums.sort()
+
+print(len(people_nums))
+
+for i in range(len(people_nums)):
+    print(people_nums[i])
+
