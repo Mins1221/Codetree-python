10df23014e4d28b194bfb08b7fd8bc369bb0c4f4
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-06 12:21:04 +0900

    [Passed] 괄호 쌍 만들어주기 (61ms, 18MB)

---
+++ 260506/괄호 쌍 만들어주기/pair-parentheses.py
@@ -0,0 +1,18 @@
+import sys
+A = input()
+INT_MAX = sys.maxsize
+n = len(A)
+R = [0] *(n+1)
+
+for i in range(n-2,-1,-1):
+    R[i] = R[i+1]
+    if A[i] ==')' and A[i+1] ==')':
+        R[i] +=1
+
+ans = 0
+for i in range(n-2):
+    if A[i] =='(' and A[i+1] =='(':
+        ans += R[i+2]
+
+print(ans)
+
