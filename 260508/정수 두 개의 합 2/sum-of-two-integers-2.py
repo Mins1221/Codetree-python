2355798a42fbf3fa1941cd9152f7f410a06b5edd
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-08 16:06:01 +0900

    [Passed] 정수 두 개의 합 2 (118ms, 19MB)

--- 260508/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 260508/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -1,16 +1,13 @@
 n, k = map(int, input().split())
-arr = [0] + [int(input()) for _ in range(n)]
+arr = [0] +  [int(input()) for _ in range(n)]
 arr.sort()
-count = 0
-left =1
-right = n
-
+ans = 0
+j = n
 for i in range(1,n+1):
-    while left < right :
-        if arr[left] + arr[right] <= k:
-            count += right - left
-            left +=1
-        else :
-            right -=1
+    while j != 1 and arr[i] + arr[j] > k :
+        j -= 1
+    if j <= i:
+        break
+    ans += j-i
 
-print(count)
+print(ans)
