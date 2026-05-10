6e2687816eb97ee86a877238d121ffed7fd54502
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-10 18:37:29 +0900

    [Wrong Answer] 0에 가장 가까운 합 (45ms, 16MB)

---
+++ trail5/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -0,0 +1,18 @@
+import sys
+n = int(input())
+a = [0] + list(map(int, input().split()))
+
+INT_MAX = sys.maxsize
+a =  sorted(a)
+
+
+ans = INT_MAX
+j = n
+for i in range(1,n+1):
+    if i < j :
+        ans = min(ans,abs(a[i]+a[j]))
+    while i < j -1 and a[i] + a[j] > 0:
+        j -=1
+        ans = min(ans,abs(a[i] + a[j]))
+
+print(ans)
