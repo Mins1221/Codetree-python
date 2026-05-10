09806857aa43c05063987e9131c64880af99d968
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-10 19:22:50 +0900

    [Runtime Error] 가장 먼저 나오는 숫자 (78ms, 18MB)

---
+++ trail5/가장 먼저 나오는 숫자/first-appear-number.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+query = list(map(int, input().split()))
+
+def lower_bound(i):
+    left = 0
+    right = n-1
+    min_idx = n
+    while left <= right:
+        mid = (left + right) //2
+        if arr[mid] >= i:
+            min_idx = min(min_idx,mid)
+            right = mid -1
+        else:
+            left = mid +1
+    if arr[min_idx] == i:
+        return min_idx +1
+    else:
+        return -1
+
+for i in query:
+    ans = lower_bound(i) 
+    print(ans)
