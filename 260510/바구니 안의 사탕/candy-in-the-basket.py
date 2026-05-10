710f4419cd19ff3c40c8c5dcb7b64da92933774d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-10 18:24:17 +0900

    [Passed] 바구니 안의 사탕 (476ms, 24MB)

--- 260510/바구니 안의 사탕/candy-in-the-basket.py
+++ 260510/바구니 안의 사탕/candy-in-the-basket.py
@@ -1,37 +1,22 @@
+# 입력 받기
+n, k = map(int, input().split())
 
-n, k = tuple(map(int, input().split()))
-candies = [(-1, -1)]
+baskets = []
 for _ in range(n):
-    cnt, x = tuple(map(int, input().split()))
-    candies.append((x, cnt))
+    cnt, x = map(int, input().split())
+    baskets.append((x, cnt))
 
-def get_pos_of_candy(candy_idx):
-    x, _ = candies[candy_idx]
-    return x
-
-def get_num_of_candy(candy_idx):
-    _, cnt = candies[candy_idx]
-    return cnt
+baskets.sort()
 
 
-candies.sort()
-ans = 0
-total_nums = 0
-j = 0
-for i in range(1, n + 1):
-    # 구간의 크기가 2K보다 같거나 작은 경우에 한하여 최대로 진행합니다.
-    while j + 1 <= n and get_pos_of_candy(j + 1) - get_pos_of_candy(i) <= 2 * k:
-        total_nums += get_num_of_candy(j + 1)
-        j += 1
-    
-    # 현재 구간 [i, j]는 
-    # i를 시작점으로 하는
-    # 가장 긴 구간이므로
-    # 구간 내 최대 사탕의 수를 갱신해줍니다.
-    ans = max(ans, total_nums)
+sum_candy = 0
+current_candy= 0
+left = 0
 
-    # 다음 구간으로 넘어가기 전에
-    # i번째에 해당하는 사탕을 구간에서 제외시킵니다.
-    total_nums -= get_num_of_candy(i)
-
-print(ans)
+for right in range(n):  # right (구간의 끝점)
+    current_candy += baskets[right][1]
+    while baskets[right][0] - baskets[left][0] > 2 * k: # 구간을 벗어나면
+        current_candy -= baskets[left][1]
+        left += 1
+    sum_candy = max(sum_candy, current_candy)
+print(sum_candy)
