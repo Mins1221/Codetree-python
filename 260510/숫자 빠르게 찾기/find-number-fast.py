f7e246e1d2f345a9557ac57660c44c0e8831d75d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-10 16:28:38 +0900

    [Passed] 숫자 빠르게 찾기 (177ms, 24MB)

---
+++ 260510/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [int(input()) for _ in range(m)]
+
+for i in queries:
+    idx = -1
+    left, right = 0, n-1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == i:
+            idx = mid +1
+            break
+        if arr[mid] > i:
+            right = mid -1
+        else:
+            left = mid + 1
+
+    print(idx)
+
