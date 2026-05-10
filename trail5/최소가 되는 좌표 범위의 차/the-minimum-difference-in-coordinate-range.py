2cf7717dab229805c94a7190451fd958cac07b66
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-10 18:41:17 +0900

    [Passed] 최소가 되는 좌표 범위의 차 (299ms, 33MB)

--- trail5/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ trail5/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -1,54 +1,41 @@
-import sys
-from sortedcontainers import SortedSet
+from heapq import heappush, heappop
+n, d = map(int, input().split())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+points.sort()
+max_heap = []
+min_heap = []
 
-n, d = tuple(map(int, input().split()))
-point = [
-    tuple(map(int, input().split()))
-    for _ in range(n)
-]
+now = dict()
 
-point = [(0, 0)] + sorted(point)
+i = 0
+MAX = float('inf')
+ans = MAX
 
-# 변수 선언
-point_count = SortedSet()
+for j in range(n):
+    cur_y = points[j][1]
+    heappush(max_heap, -cur_y)
+    heappush(min_heap, cur_y)
+    now[cur_y] = now.get(cur_y, 0) + 1
 
-def get_min():
-    if not point_count: return 0
-    return point_count[0][0]
+    while True:
+        while max_heap and now.get(-max_heap[0], 0) == 0:
+            heappop(max_heap)
+        while min_heap and now.get(min_heap[0], 0) == 0:
+            heappop(min_heap)
 
-def get_max():
-    if not point_count: return 0
-    return point_count[-1][0]
+        if not max_heap or not min_heap:
+            break
 
+        if ( -max_heap[0] - min_heap[0] ) >= d: 
+            ans = min(ans, points[j][0] - points[i][0]) 
+            now[points[i][1]] -= 1
+            i += 1
+        else:
+            break
 
-# 가능한 구간 중 최소 크기를 구합니다.
-ans = sys.maxsize
-
-# 구간을 잡아봅니다.
-j = 0
-for i in range(1, n + 1):
-    # y좌표 차가 d가 되기 전까지 계속 진행합니다.
-    while j + 1 <= n and get_max() - get_min() < d:
-        point_count.add((point[j + 1][1], point[j + 1][0]))
-        j += 1
-
-    # 만약 최대한 이동했는데도
-    # y좌표 차가 d가 되지 못했다면
-    # 탐색을 종료합니다.
-    if get_max() - get_min() < d:
-        break
-
-    # 현재 구간 [i, j]는 
-    # point[i]를 시작점으로 하는
-    # 가장 짧은 구간이므로
-    # 구간 크기 중 최솟값을 갱신합니다.
-    ans = min(ans, point[j][0]-point[i][0])
+if ans == MAX:
+    ans = -1
+print(ans)
     
-    # 다음 구간으로 넘어가기 전에
-    # point[i]에 해당하는 값은 point_count에서 지워줍니다.
-    point_count.remove((point[i][1], point[i][0]))
+    
 
-# 만약 불가능하다면
-# -1을 답으로 합니다.
-if ans == sys.maxsize: print(-1)
-else: print(ans)
