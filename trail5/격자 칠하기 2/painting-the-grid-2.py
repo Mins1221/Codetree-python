d0dbf39609333d29fc195e325badb4338a9752e0
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-14 20:10:11 +0900

    [Passed] 격자 칠하기 2 (259ms, 23MB)

--- trail5/격자 칠하기 2/painting-the-grid-2.py
+++ trail5/격자 칠하기 2/painting-the-grid-2.py
@@ -1,65 +1,58 @@
 import sys
+from collections import deque
+input = sys.stdin.readline
 
-# 재귀함수의 깊이 제한을 풀어줍니다.
-sys.setrecursionlimit(10000)
-
-# 변수 선언 및 입력:
 n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
-
-dxs = [1, -1,  0, 0]
-dys = [0,  0, -1, 1]
-
-visited = [
-    [False] * n
-    for _ in range(n)
+arr = [
+    list(map(int, input().split())) for _ in range(n)
 ]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
-# dfs를 이용해 색칠하고 몇 칸이 칠해졌는지 확인합니다.
-def dfs(x, y, d):
-    if visited[x][y]:
-        return 0
+
+def bfs(d):
+    visit = [[False] * n for _ in range(n)]
+    ans = 0
+    #시작 위치
+    for r in range(n):
+        for c in range(n):
+            if visit[r][c]:
+                continue
+
+            cnt = 0
+            queue = deque([(r, c)])
+
+            while queue:
+                y, x = queue.popleft()
+
+                if visit[y][x]:
+                    continue
+
+                cnt += 1
+                visit[y][x] = True
+
+                for i in range(4):
+                    ny, nx = y + dy[i], x + dx[i]
+
+                    if 0 <= ny < n and 0 <= nx < n and abs(arr[ny][nx] - arr[y][x]) <= d and not visit[ny][nx]:
+                        queue.append((ny, nx))
+            
+            ans = max(ans, cnt)
+            if ans >= (n * n + 1) // 2:
+                return True
     
-    visited[x][y] = True
-    count = 1
-    for dx, dy in zip(dxs, dys):
-        next_x = x + dx
-        next_y = y + dy
-        if(next_x >= 0 and next_x < n and next_y >= 0 and next_y < n
-            and abs(board[next_x][next_y] - board[x][y]) <= d):
-            count += dfs(next_x, next_y, d)
-    return count
-
-# d 이하로 인접한 칸을 색칠한다고 할 때,
-# 색칠된 칸이 전체 칸의 반 이상이 될 수 있는지 판단합니다.
-def is_possible(d):
-    # visited 배열을 초기화합니다.
-    for i in range(n):
-        for j in range(n):
-            visited[i][j] = False
-
-    # 모든 구역에 대해 절반 이상이 칠해지는 구역이 있는지 확인합니다.
-    for i in range(n):
-        for j in range(n):
-            if not visited[i][j]:
-                if dfs(i, j, d) * 2 >= n*n:
-                    return True
-
-    # 절반 이상이 칠해지는 구역이 없다면 False를 반환합니다.
     return False
 
 
-lo = 0                       # 답이 될 수 있는 가장 작은 숫자 값을 설정합니다.
-hi = 1000000                 # 답이 될 수 있는  가장 큰 숫자 값을 설정합니다.
-ans = 0                      # 답을 저장합니다.
+left, right = 0, 1000000
+ans = sys.maxsize
 
-while lo <= hi:              # [lo, hi]가 유효한 구간이면 계속 수행합니다.
-    mid = (lo + hi) // 2     # 가운데 위치를 선택합니다.
-    if is_possible(mid):     # 결정문제에 대한 답이 Yes라면
-        hi = mid - 1         # 왼쪽에 조건을 만족하는 숫자가 더 있을 가능성 때문에 hi를 바꿔줍니다.
-        ans = mid            # 답의 후보들 중 최댓값을 계속 갱신해줍니다.
+while left <= right:
+    mid = (left + right) // 2
+
+    if bfs(mid):
+        right = mid - 1
+        ans = min(ans, mid)
     else:
-        lo = mid + 1         # 결정문제에 대한 답이 No라면 lo를 바꿔줍니다.
+        left = mid + 1
 
-# 정답을 출력합니다.
 print(ans)
