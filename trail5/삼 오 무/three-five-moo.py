aed3da0c0d26ad3f733c58306b6e5ce835f3a78d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-14 17:54:09 +0900

    [Passed] 삼 오 무 (45ms, 16MB)

--- trail5/삼 오 무/three-five-moo.py
+++ trail5/삼 오 무/three-five-moo.py
@@ -1,21 +1,34 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+arr = [0 for _ in range(101)]
 
-def count(x):
-    return x - x//3 - x//5 + x//15
+l = 0
+r = 100000000000
+mid = 0
 
-def solve():
-    N = int(input())
-    lo, hi = 1, N * 2  
-    while lo < hi:
-        mid = (lo + hi) // 2
-        if count(mid) >= N:
-            hi = mid
+# print(arr)
+
+while l <= r:
+    # print(l,r)
+    mid = (l + r) // 2
+
+    # 숫자의 개수를 구하는 함수
+    cnt = 0
+
+    three = mid // 3
+    five = mid // 5
+    fifteen = mid // 15
+
+    cnt = mid - three - five + fifteen
+    # print("cnt",cnt)
+
+    if cnt < n:
+        l = mid + 1
+    elif cnt > n:
+        r = mid - 1
+    else:
+        if mid%3 == 0 or mid%5 == 0:
+            r= mid-1
         else:
-            lo = mid + 1
-    
-    print(lo)
-
-solve()
-
-
+            ans = mid
+            break
+print(ans)
