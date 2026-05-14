13dadc3f52a3a8d6e4a6bbe1ca4ec84d106036cb
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-14 17:49:11 +0900

    [Wrong Answer] 정수 분배하기 (48ms, 16MB)

---
+++ trail5/정수 분배하기/distributing-integers.py
@@ -0,0 +1,24 @@
+import sys
+INT_MIN = -sys.maxsize
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+max_sum = 0
+def is_possible(k):
+    max_sum = 0
+    for elem in arr:
+        result =elem // k 
+        max_sum += result
+
+    if max_sum >= m:
+        return True
+left = 1
+right = max(arr)
+ans = INT_MIN
+while left <= right:
+    mid = (left + right) //2
+    if is_possible(mid):
+        left = mid +1
+        ans = mid
+    else:
+        right = mid -1
+print(ans)
