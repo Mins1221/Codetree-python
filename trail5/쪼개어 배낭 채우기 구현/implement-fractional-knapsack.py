7f47a1811f29533b87140ebd563449c806eb3834
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-18 15:32:23 +0900

    [Passed] 쪼개어 배낭 채우기 구현 (92ms, 24MB)

--- trail5/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
+++ trail5/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -1,23 +1,29 @@
-# 변수 선언 및 입력:
-n, m = tuple(map(int, input().split()))
-jewels = [
-    tuple(map(int, input().split()))
-    for _ in range(n)
-]
-ans = 0
+import sys
 
-# 가치 / 무게가 내림차순이 되도록 정렬합니다.
-jewels.sort(key=lambda x: -x[1] / x[0])
+n, m = map(int, sys.stdin.readline().split())
+jewels = []
 
-for w, v in jewels:
-    # 현재 보석을 온전히 다 담을 수 있다면 그대로 담아줍니다.
-    if m >= w:
-        m -= w
-        ans += v
-    # 만약 부분만 담을 수 있다면
-    # 비율에 맞춰 담아준 뒤 종료합니다.
-    else:
-        ans += m / w * v
+for _ in range(n):
+    w, v = map(int, sys.stdin.readline().split())
+    ratio = v / w 
+    jewels.append([w, v, ratio]) 
+
+jewels.sort(key=lambda x: x[2], reverse=True) 
+
+total = 0 
+for jewel in jewels:
+    w, v = jewel[0], jewel[1]
+    
+    if m == 0:
         break
 
-print(f"{ans:.3f}")
+    else:
+        if m >= w:
+            m -= w
+            total += v
+        
+        else:
+            total += v * (m / w)
+            m = 0 
+
+print(f'{total:.3f}')
