d693956a95b9f58b28cc7130155161157a08a21d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-20 14:49:02 +0900

    [Passed] 수 합치기 (211ms, 24MB)

---
+++ trail5/수 합치기/merge-numbers.py
@@ -0,0 +1,16 @@
+import heapq
+n = int(input())
+arr = list(map(int, input().split()))
+
+pq = []
+ans = 0
+for elem in arr:
+    heapq.heappush(pq, elem)
+while len(pq) > 1:
+    x1 = heapq.heappop(pq)
+    x2 = heapq.heappop(pq)
+    ans += (x1 + x2)
+    heapq.heappush(pq, x1 + x2)
+
+print(ans)
+
