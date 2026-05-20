632272704833346da6077baa046e73cca42d2321
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-20 14:53:51 +0900

    [Passed] G & H 반전시키기 (57ms, 16MB)

---
+++ trail5/G & H 반전시키기/reversing-g-and-h.py
@@ -0,0 +1,17 @@
+N = int(input())
+a = input()
+b = input()
+
+# Please write your code here.
+ans = 0
+mismatched = False
+for i in range(N):
+    if a[i] != b[i]:
+        if not mismatched:
+            mismatched = True
+            ans += 1
+       
+    else:
+        mismatched = False
+
+print(ans)
