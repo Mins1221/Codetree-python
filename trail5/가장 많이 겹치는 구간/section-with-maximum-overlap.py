828eef676491d151d33a0eecd1dbd3670718d410
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-29 17:51:10 +0900

    [Passed] 가장 많이 겹치는 구간 (153ms, 21MB)

---
+++ trail5/가장 많이 겹치는 구간/section-with-maximum-overlap.py
@@ -0,0 +1,15 @@
+n = int(input())
+intervals = [tuple(map(int, input().split())) for _ in range(n)]
+points = []
+for x1,x2 in intervals:
+    points.append((x1,+1))
+    points.append((x2,-1))
+
+points.sort()
+count = 0
+sum_val = 0
+for x,v in points:
+    sum_val += v
+    count = max(count,sum_val)
+
+print(count)
