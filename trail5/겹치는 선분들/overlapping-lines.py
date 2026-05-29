cfc5624d48db3e5f85aadc880598667eddb42b9d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-05-29 19:41:23 +0900

    [Passed] 겹치는 선분들 (784ms, 33MB)

--- trail5/겹치는 선분들/overlapping-lines.py
+++ trail5/겹치는 선분들/overlapping-lines.py
@@ -2,6 +2,7 @@ n,k = map(int,input().split())
 result = 0
 x = 0
 points = []
+
 for _ in range(n):
     m,d = tuple(input().split())
     m = int(m)
@@ -11,19 +12,17 @@ for _ in range(n):
     else:
         points.append((x-m,x))
         x -=m
-
 positions = []
 for x1,x2 in points:
     positions.append((x1,+1))
     positions.append((x2,-1))
 
 positions.sort()
-
 sum_val = 0
 for i, (x,v) in enumerate(positions):
-    if sum_val >= k :
-        pre_v, _ = positions[i-1]
-        result += x - pre_v
+    if sum_val >= k:
+        pre_x , _ = positions[i-1]
+        result += x -pre_x 
     sum_val +=v
 
 print(result)
