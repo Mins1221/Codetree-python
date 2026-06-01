2e4bb7e861911cfed237e6b66e9fb96327049c6e
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 15:59:39 +0900

    [Wrong Answer] 각 정점까지의 최단 경로 3 (50ms, 16MB)

--- trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
+++ trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -1,29 +1,28 @@
-n, m = map(int, input().split())
-edges = [0] + [tuple(map(int, input().split())) for _ in range(m)]
-graph = [[0] * (n + 1) for _ in range(n + 1)]
+n,m = map(int,input().split())
+edges = [tuple(map(int,input().split())) for _ in range(m)]
 import sys
 INT_MAX = sys.maxsize
-visited = [False] * (n + 1)
-dist = [INT_MAX] * (n + 1)
-
-for i in range(1, m + 1):
-    x, y, z = edges[i]
-    graph[x][y] = z
+graph = [[0] *(n+1) for _ in range(n+1)]
+dist = [INT_MAX] * ( n+1)
+visited = [False] *(n+1)
 
 dist[1] = 0
+for i in range(n):
+    x,y,z = edges[i]
+    graph[x][y] = z
 
-for i in range(1, n + 1):
+for i in range(1,n+1):
     min_index = -1
-    for j in range(1, n + 1):
-        if visited[j]:
+    for j in range(1,n+1):
+        if visited[j] :
             continue
-        if min_index == -1 or dist[min_index] > dist[j]:
+        if min_index == -1 or dist[min_index] > dist[j] :
             min_index = j
     visited[min_index] = True
-    for j in range(1, n + 1):
-        if graph[min_index][j] == 0:
+    for j in range(1,n+1):
+        if graph[min_index][0] == 0:
             continue
         dist[j] = min(dist[j], dist[min_index] + graph[min_index][j])
 
-for i in range(2, n + 1):
-    print(-1 if dist[i] == INT_MAX else dist[i])  # ✅ 여기서 처리
+for i in range(2,n+1):
+    print(-1 if dist[i] == INT_MAX else dist[i])
