20f74bfbc4d0e8458bc63c266ee6a164446ec8e4
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 16:39:12 +0900

    [Passed] 각 정점까지의 최단 경로 (632ms, 59MB)

--- trail5/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ trail5/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -1,21 +1,19 @@
-n, m = map(int, input().split())
+n,m = map(int,input().split())
 k = int(input())
-edges = [0] + [tuple(map(int, input().split())) for _ in range(m)]
-import heapq
+edges = [0] + [tuple(map(int,input().split())) for _ in range(m)]
 import sys
+import heapq
 INT_MAX = sys.maxsize
-graph = [[] for _ in range(n + 1)]
 pq = []
-dist = [INT_MAX] * (n + 1)
+graph = [[] for _ in range(n+1)]
+dist= [INT_MAX] * (n+1)
 
-for i in range(1, m + 1):
-    x, y, z = edges[i]
-    graph[x].append((y, z))
-    graph[y].append((x, z))  # ✅ 무방향이므로 역방향 추가
-
+for i in range(1,m+1):
+    x,y,z = edges[i]
+    graph[x].append((y,z))
+    graph[y].append((x,z))
 dist[k] = 0
-heapq.heappush(pq, (0, k))  # ✅ 5 → k
-
+heapq.heappush(pq,(0,k))
 while pq:
     min_dist, min_index = heapq.heappop(pq)
     if min_dist != dist[min_index]:
@@ -24,7 +22,7 @@ while pq:
         new_dist = dist[min_index] + target_dist
         if dist[target_index] > new_dist:
             dist[target_index] = new_dist
-            heapq.heappush(pq, (new_dist, target_index))
+            heapq.heappush(pq,(new_dist,target_index))
 
-for i in range(1, n + 1):
-    print(-1 if dist[i] == INT_MAX else dist[i])
+for i in range(1,n+1):
+    print(-1 if dist[i] ==INT_MAX else dist[i])
