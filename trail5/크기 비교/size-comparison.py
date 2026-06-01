758ce5b6d50db1ec8786a41e41a1a8b5452748cb
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 20:31:45 +0900

    [Passed] 크기 비교 (145ms, 19MB)

---
+++ trail5/크기 비교/size-comparison.py
@@ -0,0 +1,20 @@
+n, m = tuple(map(int, input().split()))
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+    graph[x][y] = 1
+
+for k in range(1, n + 1): 
+    for i in range(1, n + 1): 
+        for j in range(1, n + 1):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+for i in range(1, n + 1):
+    cnt = 0
+    for j in range(1, n + 1):
+        if i == j:
+            continue
+        if graph[i][j] or graph[j][i]:
+            continue                 
+        cnt += 1
+    print(cnt)
