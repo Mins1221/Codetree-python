def1571e915794070f6e6af5e0af74ff4301ba01
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 20:02:03 +0900

    [Passed] 최단 거리 3 (180ms, 25MB)

--- trail5/최단 거리 3/shortest-distance-3.py
+++ trail5/최단 거리 3/shortest-distance-3.py
@@ -1,34 +1,29 @@
-import heapq
-
+import sys, heapq
+INT_MAX = sys.maxsize
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 A, B = map(int, input().split())
+graph = [[] * (n + 1) for _ in range(n + 1)]
+for edge in edges:
+    a, b, c = edge
+    graph[a].append((b, c))
+    graph[b].append((a, c))
 
-graph = [[] for _ in range(n+1)]
+dist = [INT_MAX] * (n + 1)
+dist[A] = 0
+pq = []
+heapq.heappush(pq, (0, A))
+while pq:
+    min_dist, min_index = heapq.heappop(pq)
 
-for x, y, z in edges:
-    graph[x].append((y, z))
-    graph[y].append((x, z))
-
-def dijkstra(start):
-    dist = [float('inf')] * (n+1)
-    dist[A] = 0
-
-    pq = []
-    heapq.heappush(pq, (0, start))
-
-    while pq:
-        cost, node = heapq.heappop(pq)
-
-        if dist[node] < cost:
-            continue
-
-        for next_node, weight in graph[node]:
-            if dist[next_node] >= cost + weight:
-                dist[next_node] = cost + weight
-                heapq.heappush(pq, (dist[next_node], next_node))
-    return dist[B]
+    if min_dist > dist[min_index]:
+        continue 
+    for target_index, target_dist in graph[min_index]:
+        new_dist = min_dist + target_dist
+        if dist[target_index] > new_dist:
+            dist[target_index] = new_dist
+            heapq.heappush(pq, (new_dist, target_index))
+print(dist[B])
 
 
-print(dijkstra(A))
 
