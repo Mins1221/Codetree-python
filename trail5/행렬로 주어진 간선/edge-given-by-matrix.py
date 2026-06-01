0c59820f6a84bd0bc0cf4fcabab20f682676b9f2
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 20:27:50 +0900

    [Passed] 행렬로 주어진 간선 (54ms, 16MB)

---
+++ trail5/행렬로 주어진 간선/edge-given-by-matrix.py
@@ -0,0 +1,18 @@
+n = int(input())
+graph = [[0] * (n + 1)for _ in range(n + 1)]
+for i in range(1, n + 1):
+    graph[i] = [0] + list(map(int, input().split()))
+    
+for i in range(1, n + 1):
+    graph[i][i] = 1
+
+for k in range(1, n + 1): 
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(graph[i][j], end=" ")
+    print()
