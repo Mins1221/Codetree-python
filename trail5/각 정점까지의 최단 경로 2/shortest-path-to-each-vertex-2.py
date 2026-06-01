4b2174ea916b9dc91998c46fa25803db660bacbd
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 20:13:20 +0900

    [Passed] 각 정점까지의 최단 경로 2 (209ms, 28MB)

--- trail5/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
+++ trail5/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -20,7 +20,7 @@ for k in range(1, n + 1):
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        if dist[i][j] == INF:
+        if dist[i][j] == INT_MAX:
             print(-1, end = " ")
         else:
             print(dist[i][j], end = " ")
