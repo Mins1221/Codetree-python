3b7ce37794be17553219e28a0c3b1244139a6bfe
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 16:05:49 +0900

    [Passed] 각 정점까지의 최단 경로 3 (85ms, 17MB)

--- trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
+++ trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -1,5 +1,5 @@
 n,m = map(int,input().split())
-edges = [tuple(map(int,input().split())) for _ in range(m)]
+edges = [0]+ [tuple(map(int,input().split())) for _ in range(m)]
 import sys
 INT_MAX = sys.maxsize
 graph = [[0] *(n+1) for _ in range(n+1)]
@@ -7,7 +7,7 @@ dist = [INT_MAX] * ( n+1)
 visited = [False] *(n+1)
 
 dist[1] = 0
-for i in range(n):
+for i in range(1,m+1):
     x,y,z = edges[i]
     graph[x][y] = z
 
@@ -20,7 +20,7 @@ for i in range(1,n+1):
             min_index = j
     visited[min_index] = True
     for j in range(1,n+1):
-        if graph[min_index][0] == 0:
+        if graph[min_index][j] == 0:
             continue
         dist[j] = min(dist[j], dist[min_index] + graph[min_index][j])
 
