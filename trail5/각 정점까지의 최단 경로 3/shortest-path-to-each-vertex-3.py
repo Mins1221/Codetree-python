08373b3e49c584d7c226c889dad6b60830ac0240
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-01 15:21:08 +0900

    [Passed] 각 정점까지의 최단 경로 3 (82ms, 17MB)

--- trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
+++ trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -1,31 +1,29 @@
 n, m = map(int, input().split())
-edges =[0]+ [tuple(map(int, input().split())) for _ in range(m)]
-graph = [[0] *(n+1) for _ in range(n+1)]
+edges = [0] + [tuple(map(int, input().split())) for _ in range(m)]
+graph = [[0] * (n + 1) for _ in range(n + 1)]
 import sys
 INT_MAX = sys.maxsize
 visited = [False] * (n + 1)
-dist = [INT_MAX] * (n + 1) 
+dist = [INT_MAX] * (n + 1)
+
 for i in range(1, m + 1):
     x, y, z = edges[i]
     graph[x][y] = z
+
 dist[1] = 0
+
 for i in range(1, n + 1):
     min_index = -1
     for j in range(1, n + 1):
         if visited[j]:
             continue
-        
         if min_index == -1 or dist[min_index] > dist[j]:
             min_index = j
     visited[min_index] = True
     for j in range(1, n + 1):
         if graph[min_index][j] == 0:
             continue
-    
         dist[j] = min(dist[j], dist[min_index] + graph[min_index][j])
-if dist[j] == INT_MAX:
-    dist[j] = -1
+
 for i in range(2, n + 1):
-    print(dist[i])
-
-
+    print(-1 if dist[i] == INT_MAX else dist[i])  # ✅ 여기서 처리
