7b3d5f07a20b7aed359fef29e7c6adf9942c4c85
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-02 08:56:35 +0900

    [Passed] 가장 짧은 부분합 (58ms, 18MB)

--- trail5/가장 짧은 부분합/shortest-subtotal.py
+++ trail5/가장 짧은 부분합/shortest-subtotal.py
@@ -3,18 +3,18 @@ arr = list(map(int, input().split()))
 import sys
 INT_MAX = sys.maxsize
 j = 0
-sum_val = arr[0]
 ans = INT_MAX
+sum_val = arr[0]
 for i in range(n):
     while j +1 < n and sum_val < s:
         sum_val += arr[j+1]
         j +=1
-
     if sum_val >= s:
         ans = min(ans,j-i+1)
 
     sum_val -= arr[i]
 
-if ans == INT_MAX:
+if ans ==INT_MAX:
     ans = -1
+
 print(ans)
