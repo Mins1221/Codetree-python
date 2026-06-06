5e32ca5074ef7c1983a7ef9e9842e89c1863116f
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-06 17:16:20 +0900

    [Passed] 자연수 n개의 합 (62ms, 16MB)

--- trail5/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ trail5/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -1,17 +1,15 @@
 import sys
 INT_MIN = -sys.maxsize
 s = int(input())
-
-
-left = 1
 right = s
-max_num = INT_MIN
-while left <= right:
-    mid = (left + right) //2
-    if mid * (mid +1) //2 <= s:
+left = 0
+ans = INT_MIN
+while left <= right :
+    mid = (left+right) // 2
+    if (mid * (mid +1) // 2) <= s:
         left = mid +1
-        max_num = max(max_num,mid)
+        ans = max(ans,mid)
     else:
         right = mid -1
 
-print(max_num)
+print(ans)
