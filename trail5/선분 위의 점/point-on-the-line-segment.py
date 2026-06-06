75863301fde6a5d3579f22b8c700279a23593b1e
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-06 16:44:06 +0900

    [Passed] 선분 위의 점 (394ms, 37MB)

--- trail5/선분 위의 점/point-on-the-line-segment.py
+++ trail5/선분 위의 점/point-on-the-line-segment.py
@@ -1,30 +1,11 @@
 n, m = map(int, input().split())
-points = list(map(int, input().split()))
-segments = [tuple(map(int, input().split())) for _ in range(m)]
+arr = list(map(int, input().split()))
+lines = [list(map(int, input().split())) for _ in range(m)]
 
-# Please write your code here.
-points.sort()
-def lower_bound(q):
-    l, r = 0, n-1
-    while l<=r:
-        mid = (l+r)//2
-        if q <= points[mid]:
-            r = mid-1
-        else:
-            l = mid+1
-    return l
+arr = sorted(arr)
+import bisect
 
-def upper_bound(q):
-    l, r = 0, n-1
-    while l<=r:
-        mid = (l+r)//2
-        if q < points[mid]:
-            r = mid-1
-        else:
-            l = mid+1
-    return r
-
-for x, y in segments:
-    left = lower_bound(x)
-    right = upper_bound(y)
-    print(right-left+1)
+for s, e in lines:
+    l = bisect.bisect_left(arr, s)
+    r = bisect.bisect_right(arr, e)
+    print(r-l) 
