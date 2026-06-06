8675378b3d400e5b02f772eaf94d20a859e91acf
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-06 17:17:46 +0900

    [Passed] 정수 분배하기 (71ms, 17MB)

--- trail5/정수 분배하기/distributing-integers.py
+++ trail5/정수 분배하기/distributing-integers.py
@@ -1,24 +1,15 @@
-import sys
-INT_MIN = -sys.maxsize
-n, m = map(int, input().split())
-arr = [int(input()) for _ in range(n)]
-max_sum = 0
-def is_possible(k):
-    max_sum = 0
-    for elem in arr:
-        result =elem // k 
-        max_sum += result
-
-    if max_sum >= m:
-        return True
-left = 1
-right = max(arr)
+N, M = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+l, r = 1, max(arr)
 ans = 0
-while left <= right:
-    mid = (left + right) //2
-    if is_possible(mid):
-        left = mid +1
+while l <= r:
+    mid = (l + r) // 2
+    cnt = sum(x // mid for x in arr)
+
+    if cnt >= M:
         ans = mid
+        l = mid + 1
     else:
-        right = mid -1
+        r = mid - 1
+
 print(ans)
