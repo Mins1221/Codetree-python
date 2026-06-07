a08135edf39d0510cecf1fe594c50a75de0ee3d7
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-07 16:33:06 +0900

    [Passed] 삼 오 무 (52ms, 16MB)

--- trail5/삼 오 무/three-five-moo.py
+++ trail5/삼 오 무/three-five-moo.py
@@ -1,34 +1,16 @@
+import sys
+INT_MAX = sys.maxsize
 n = int(input())
-arr = [0 for _ in range(101)]
-
 l = 0
-r = 100000000000
-mid = 0
-
-# print(arr)
+r = INT_MAX
+ans = INT_MAX
 
 while l <= r:
-    # print(l,r)
-    mid = (l + r) // 2
-
-    # 숫자의 개수를 구하는 함수
-    cnt = 0
-
-    three = mid // 3
-    five = mid // 5
-    fifteen = mid // 15
-
-    cnt = mid - three - five + fifteen
-    # print("cnt",cnt)
-
-    if cnt < n:
-        l = mid + 1
-    elif cnt > n:
-        r = mid - 1
+    mid = (l+r) // 2
+    if mid - mid // 3 - mid // 5 + mid // 15 >=n:
+        r = mid -1
+        ans = min(ans,mid)
     else:
-        if mid%3 == 0 or mid%5 == 0:
-            r= mid-1
-        else:
-            ans = mid
-            break
+        l = mid + 1
+
 print(ans)
