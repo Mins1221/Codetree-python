62c681fc138654e9e7d79f932da54629f2791732
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-07 20:35:55 +0900

    [Passed] 이차원 배열의 오름차순 정리 (61ms, 17MB)

--- trail5/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.py
+++ trail5/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.py
@@ -1,19 +1,17 @@
 n = int(input())
 k = int(input())
-
 high = n*n
-ans = n* n
-low = 1
-while low <= high :
-    mid = (high +low) // 2
-    val = 0
-    for i in range(1,n+1):
-        val +=min(n,mid // i)
+ans = n*n
+low =1
 
-    if val >= k:
-        high = mid  -1
-        ans = min(ans,mid)
+while low <= high:
+    mid = (low+high) // 2
+    val =0
+    for i in range(1,n+1):
+        val += min(n,mid//i)
+    if val>= k :
+        high = mid -1
+        ans = min(mid,ans)
     else:
         low = mid +1
-
 print(ans)
