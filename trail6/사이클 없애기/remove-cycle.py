853ed534cf6a867c2aefd8da119fa735503e51ab
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-08 18:55:29 +0900

    [Passed] 사이클 없애기 (394ms, 34MB)

--- trail6/사이클 없애기/remove-cycle.py
+++ trail6/사이클 없애기/remove-cycle.py
@@ -1,32 +1,25 @@
 from collections import deque
-n, m1, m2 = tuple(map(int, input().split()))
-edges = [[] for _ in range(n + 1)]
-indegree = [0] * (n + 1)
-visited = [False] * (n + 1)
-q = deque()
-for _ in range(m1):
-    x, y = tuple(map(int, input().split()))
+n, m1, m2 = map(int, input().split())
+edges = [[] for _ in range(n+1)]
+indegree = [0] * (n+1)
+for i in range(m1):
+    x,y = map(int, input().split())
+    edges[x].append(y)
+    indegree[y]+=1
 
-    edges[x].append(y) 
-    indegree[y] += 1
-for _ in range(m2):
-    a, b = tuple(map(int, input().split()))
-for i in range(1, n + 1):
+for i in range(m2):
+    x,y = map(int, input().split())
+q = deque()
+for i in range(1, n+1):
     if not indegree[i]:
         q.append(i)
+ans = []
 while q:
-    x = q.popleft()
+    now = q.popleft()
+    ans.append(now)
+    for x in edges[now]:
+        indegree[x] -= 1
+        if not indegree[x]:
+            q.append(x)
 
-    visited[x] = True
-    for y in edges[x]:
-        indegree[y] -= 1
-        if not indegree[y]:
-            q.append(y)
-is_cycle = False
-for i in range(1, n + 1):
-    if not visited[i]: 
-        is_cycle = True
-if is_cycle:
-    print("No")
-else:
-    print("Yes")
+print('Yes' if len(ans) == n else 'No')
