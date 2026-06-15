d1190c8fd1b353419b9aa56926837b58464cff40
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-15 15:12:51 +0900

    [Runtime Error] 트리의 부모 노드 (110ms, 18MB)

--- trail6/트리의 부모 노드/parent-node-of-the-tree.py
+++ trail6/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,10 +1,11 @@
+import sys
+# input = sys.stdin.readline
+
+tree_parent = {}
 n = int(input())
 
-parent = {}
-
-for _ in range(n-1):
+for i in range(n-1):
     a, b = map(int, input().split())
-    parent[b] = a
-
-for a in range(2, n+1):
-    print (parent[a])
+    tree_parent[b] = a
+for i in range(2, n + 1):
+    print(tree_parent[i])
