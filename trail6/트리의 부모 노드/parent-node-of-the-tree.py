92a090e18365dbe19bdaaa4fdb1e1c29edf868ca
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-15 15:13:15 +0900

    [Passed] 트리의 부모 노드 (422ms, 41MB)

--- trail6/트리의 부모 노드/parent-node-of-the-tree.py
+++ trail6/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,11 +1,24 @@
 import sys
-# input = sys.stdin.readline
-
-tree_parent = {}
+sys.setrecursionlimit(100000)
 n = int(input())
+edges = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+parent = [0] * (n + 1)
 
-for i in range(n-1):
-    a, b = map(int, input().split())
-    tree_parent[b] = a
+for _ in range(n - 1):
+    x, y = tuple(map(int, input().split()))
+    edges[x].append(y)
+    edges[y].append(x)
+
+def traversal(x):
+    for y in edges[x]:
+        if not visited[y]:
+            visited[y] = True
+            parent[y] = x
+            traversal(y)
+visited[1] = True
+traversal(1)
+
 for i in range(2, n + 1):
-    print(tree_parent[i])
+    print(parent[i])
+
