b6eee017873d22a2fddc3d320e0a5c8147833297
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-15 15:08:35 +0900

    [Runtime Error] 트리의 부모 노드 (97ms, 18MB)

--- trail6/트리의 부모 노드/parent-node-of-the-tree.py
+++ trail6/트리의 부모 노드/parent-node-of-the-tree.py
@@ -1,23 +1,7 @@
-import sys
-sys.setrecursionlimit(100000)
 n = int(input())
-edges = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-parent = [0] * (n + 1)
-
-for _ in range(n - 1):
-    x, y = tuple(map(int, input().split()))
-    edges[x].append(y)
-    edges[y].append(x)
-
-def traversal(x):
-    for y in edges[x]:
-        if not visited[y]:
-            visited[y] = True
-            parent[y] = x
-            traversal(y)
-visited[1] = True
-traversal(1)
-
-for i in range(2, n + 1):
-    print(parent[i])
+parent = {}
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    parent[b] = a
+for a in range(2, n+1):
+    print (parent[a])
