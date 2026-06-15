f8b46843fb675d46690dc8becc827a90fe05df2e
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-15 16:46:01 +0900

    [Passed] 트리 정점 거리 (427ms, 47MB)

--- trail6/트리 정점 거리/node-distance.py
+++ trail6/트리 정점 거리/node-distance.py
@@ -1,31 +1,34 @@
-n, m = map(int, input().split())
-edges = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-dist = [[0] * (n + 1) for _ in range(n + 1)]
+from sys import stdin
 
+input = stdin.readline
+
+
+def dfs(s, e, d):
+    global dist
+
+    if s == e:
+        dist = d
+        return
+
+    for nxt_n, nxt_d in vertex[s]:
+        if visited[nxt_n]:
+            visited[nxt_n] = False  # 방문 처리
+            dfs(nxt_n, e, d + nxt_d)
+
+
+n, m = map(int, input().split())  # n: 정점의 개수, m: 정점 쌍의 개수
+
+vertex = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
-    x, y, d = tuple(map(int, input().split()))
-
-    edges[x].append((y, d))
-    edges[y].append((x, d))
-
-def dfs(st, x):
-    for y, d in edges[x]:
-        if visited[y]:
-            continue
-        
-        visited[y] = True
-
-        dist[st][y] = dist[st][x] + d
-        dfs(st, y)
-
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        visited[j] = False
-
-    visited[i] = True
-    dfs(i, i)
+    v1, v2, w = map(int, input().split())  # v1 <-> v2: 가중치 w
+    vertex[v1].append((v2, w))
+    vertex[v2].append((v1, w))
 
 for _ in range(m):
-    x, y = tuple(map(int, input().split()))
-    print(dist[x][y])
+    s, e = map(int, input().split())
+    visited = [True] * (n + 1)
+    visited[s] = False  # 출발 지점 방문 처리
+    dist = 0
+    dfs(s, e, 0)
+
+    print(dist)
