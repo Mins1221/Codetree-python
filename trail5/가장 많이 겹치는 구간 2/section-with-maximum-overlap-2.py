d0115384f6530e575a8d48d455dc76d681906b02
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-20 19:59:03 +0900

    [Runtime Error] 가장 많이 겹치는 구간 2 (151ms, 33MB)

--- trail5/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.py
+++ trail5/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.py
@@ -1,14 +1,15 @@
-n = int(input())
-intervals = [map(int,input().split()) for _ in range(n)]
 N = 1000000
-point= [0] * N
+n= int(input())
+intervals =[map(int,input().split()) for _ in range(n)]
+point = [0] * N
+count = 0
+sum_val = 0
 for x1,x2 in intervals:
     point[x1] +=1
     point[x2] -=1
 
-sum_val = 0
-count = 0
 for x in range(1,N):
     sum_val += point[x]
     count = max(count,sum_val)
+
 print(count)
