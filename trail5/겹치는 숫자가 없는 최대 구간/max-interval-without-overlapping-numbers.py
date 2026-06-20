2f45bc6d2a604d61b52a2ced6e8ae90a0e5c069b
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-20 21:12:45 +0900

    [Passed] 겹치는 숫자가 없는 최대 구간 (95ms, 28MB)

--- trail5/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
+++ trail5/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -1,11 +1,11 @@
 n = int(input())
 arr = [0] + list(map(int,input().split()))
-j =0
-ans =0
-count = [0] * (max(arr)+1)
+j = 0
+ans = 0 
+count = [0] * (max(arr) +1)
 for i in range(1,n+1):
-    while j+1 < n and count[arr[j+1]] !=1:
-        count[arr[j+1]] +=1
+    while j+1 <=n and count[arr[j+1]] != 1:
+        count[arr[j+1]] += 1
         j +=1
     ans = max(ans,j-i+1)
     count[arr[i]] -=1
