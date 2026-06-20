725a4ed56267f20aab48d8798d34a3f01494b559
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-20 21:26:38 +0900

    [Passed] 부분수열 여부 판단하기 (69ms, 23MB)

--- trail5/부분수열 여부 판단하기/determine-subsequence.py
+++ trail5/부분수열 여부 판단하기/determine-subsequence.py
@@ -6,8 +6,8 @@ def sub():
     i = 1
     for j in range(1,m+1):
         while i <= n and A[i] != B[j]:
-            i +=1
-        if i == n +1:
+            i+=1
+        if i == n+1:
             return False
         else:
             i+=1
