d10bbd1ba8b41275b5a36e8170191d67159c9711
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 20:51:40 +0900

    [Passed] 친구의 키 2 (161ms, 23MB)

--- trail6/친구의 키 2/height-of-friends-2.py
+++ trail6/친구의 키 2/height-of-friends-2.py
@@ -1,10 +1,10 @@
 from collections import deque
-n, m = map(int, input().split())
+n,m = map(int,input().split())
 edges = [[] for _ in range(n+1)]
-indegree = [0] * (n+1)
-q = deque()
-count =0
-for i in range(m):
+indegree = [0] *(n+1)
+q =deque()
+count = 0
+for _ in range(m):
     x,y = map(int,input().split())
     edges[x].append(y)
     indegree[y] +=1
@@ -12,16 +12,14 @@ for i in range(1,n+1):
     if not indegree[i]:
         q.append(i)
 while q:
-    x= q.popleft()
-    count+=1
+    x = q.popleft()
+    count +=1
     for y in edges[x]:
-        indegree[y] -= 1
+        indegree[y] -=1
         if not indegree[y]:
             q.append(y)
 
-if count ==n:
+if count == n:
     print("Consistent")
 else:
     print("Inconsistent")
-
-
