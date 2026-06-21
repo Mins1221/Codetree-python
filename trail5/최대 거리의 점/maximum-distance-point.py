a1d7d9d2c59003a001cc41b932932eb2bdd87e3d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 15:56:02 +0900

    [Passed] 최대 거리의 점 (210ms, 26MB)

--- trail5/최대 거리의 점/maximum-distance-point.py
+++ trail5/최대 거리의 점/maximum-distance-point.py
@@ -6,19 +6,19 @@ def calc(point,m,d):
     last = point[0]
     count = 1
     for p in point[1:]:
-        if p - last >= d:
-            last = p
+        if p-last >= d:
             count +=1
+            last = p
             if count >= m:
                 return True
     return False
 point.sort()
-low,high = 1, point[-1] - point[0]
-ans = 0
-while low <= high :
-    mid = (low + high) // 2
+low,high = 1,point[-1] - point[0]
+ans =0
+while low <= high:
+    mid = (low+high) // 2
     if calc(point,m,mid):
-        ans =mid
+        ans = mid
         low = mid +1
     else:
         high = mid -1
