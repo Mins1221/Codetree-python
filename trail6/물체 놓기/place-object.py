43f6fd144d8b426c6096d152d74e03477d3a5d39
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 20:21:04 +0900

    [Runtime Error] 물체 놓기 (110ms, 19MB)

--- trail6/물체 놓기/place-object.py
+++ trail6/물체 놓기/place-object.py
@@ -1,32 +1,19 @@
-import sys
-INT_MAX = sys.maxsize
 n = int(input())
-graph = [[0] * (n+1) for _ in range(n+1)]
-visited = [False] * (n+1)
+direct = [int(input()) for _ in range(n)]
+cost = [list(map(int, input().split())) for _ in range(n)]
+min_cost = direct[:]
+answer = 0
+for _ in range(n):
+    u = -1
+    best = 10 ** 18
+    for i in range(n):
+        if not visited[i] and min_cost[i] < best:
+            best = min_cost[i]
+            u = i
+    visited[u] = True
+    answer += best
+    for v in range(n):
+        if not visited[v] and cost[u][v] < min_cost[v]:
+            min_cost[v] = cost[u][v]
+print(answer)
 
-dist = [INT_MAX] * (n+1)
-
-for i in range(1,n+1):
-    graph[0][i] = int(input())
-
-for i in range(1,n+1):
-    graph[i] = [0] + list(map(int,input().split()))
-
-dist[0] = 0 
-
-ans = 0
-for i in range(0,n+1):
-    min_index = -1
-    for j in range(0,n+1):
-        if visited[j]:
-            continue
-        if min_index == -1 or dist[min_index] > dist[j]:
-            min_index = j
-
-    visited[min_index] = True
-    ans += dist[min_index]
-
-    for j in range(1,n+1):
-        dist[j] = min(dist[j],graph[min_index][j])
-
-print(ans)
