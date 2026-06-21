05fb49abd1f48799db7ad880627654e96910eb59
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 18:01:24 +0900

    [Passed] 격자 위의 연결 (1231ms, 62MB)

---
+++ trail6/격자 위의 연결/connections-on-the-grid.py
@@ -0,0 +1,43 @@
+n, m = map(int, input().split())
+
+edges = []
+uf = [0] * (n*m+1)
+for i in range(1,n+1):
+    costs = list(map(int,input().split()))
+    for j in range(1,m):
+        x = (i-1) *m+j
+        y= (i-1) *m+j+1
+
+        edges.append((costs[j-1],x,y))
+
+for i in range(1,n):
+    costs = list(map(int,input().split()))
+    for j in range(1,m+1):
+        x = (i-1) * m + j
+        y = i *m +j
+
+        edges.append((costs[j-1],x,y))
+
+def find(x):
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+
+def union(x,y):
+    X,Y = find(x), find(y)
+    uf[X] = Y
+
+edges.sort()
+for i in range(1,n*m+1):
+    uf[i] = i
+
+ans = 0
+for cost,x,y in edges:
+    if find(x) != find(y):
+        ans += cost
+        union(x,y)
+
+print(ans) 
+
+
