f085e9fdb50cd231c2a0c0f65972944725c65711
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 20:48:25 +0900

    [Passed] 친구의 키 (214ms, 24MB)

--- trail6/친구의 키/height-of-friends.py
+++ trail6/친구의 키/height-of-friends.py
@@ -1,18 +1,18 @@
 from collections import deque
 n,m = map(int,input().split())
-indegree = [0]*(n+1)
 edges = [[] for _ in range(n+1)]
+indegree = [0]*(n+1)
 q = deque()
-for i in range(m):
-    x,y = map(int,input().split())
+for _ in range(m):
+    x,y =map(int,input().split())
     edges[x].append(y)
     indegree[y] +=1
 for i in range(1,n+1):
     if not indegree[i]:
         q.append(i)
 while q:
-    x=q.popleft()
-    print(x, end=" ")
+    x = q.popleft()
+    print(x, end =" ")
     for y in edges[x]:
         indegree[y] -=1
         if not indegree[y]:
