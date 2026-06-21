af13fe17f685adf8f960d5b3d837f70edee29acc
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 21:16:25 +0900

    [Passed] 갈 수 있는 경우의 수 (134ms, 22MB)

--- trail6/갈 수 있는 경우의 수/possible-path-of-travel.py
+++ trail6/갈 수 있는 경우의 수/possible-path-of-travel.py
@@ -1,35 +1,23 @@
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-
-# Please write your code here.
 from collections import deque
-
 MOD = 1000000007
-
 graph = [[] for _ in range(n + 1)]
 indegree = [0] * (n + 1)
-
 for x, y in edges:
     graph[x].append(y)
     indegree[y] += 1
-
 q = deque()
-
 for i in range(1, n + 1):
     if indegree[i] == 0:
         q.append(i)
-
 dp = [0] * (n + 1)
 dp[1] = 1
-
 while q:
     cur = q.popleft()
-
     for nxt in graph[cur]:
         dp[nxt] = (dp[nxt] + dp[cur]) % MOD
         indegree[nxt] -= 1
-
         if indegree[nxt] == 0:
             q.append(nxt)
-
 print(dp[n])
