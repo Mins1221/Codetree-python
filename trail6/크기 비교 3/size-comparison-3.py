5e00c8a6598502734ca1825cad2b5965063876fe
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 20:58:21 +0900

    [Passed] 크기 비교 3 (297ms, 27MB)

--- trail6/크기 비교 3/size-comparison-3.py
+++ trail6/크기 비교 3/size-comparison-3.py
@@ -1,20 +1,19 @@
 import heapq
-n, m = map(int, input().split())
-edges = [[] for _ in range(n+1)]
-indegree = [0] * (n+1)
 pq = []
+n,m = map(int,input().split())
+edges = [[] for _ in range(n+1)]
+indegree = [0] *(n+1)
 for _ in range(m):
-    x,y = tuple(map(int,input().split()))
+    x,y =map(int,input().split())
     edges[x].append(y)
     indegree[y] +=1
 for i in range(1,n+1):
     if not indegree[i]:
         heapq.heappush(pq,i)
-
 while pq:
-    x= heapq.heappop(pq)
+    x = heapq.heappop(pq)
     print(x,end=" ")
     for y in edges[x]:
-        indegree[y] -= 1
+        indegree[y] -=1
         if not indegree[y]:
             heapq.heappush(pq,y)
