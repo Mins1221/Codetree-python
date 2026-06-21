ad568df3d3bc15bb744653e467fbdea90a524ac7
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 13:30:34 +0900

    [Passed] 숫자 빠르게 찾기 (170ms, 24MB)

--- trail5/숫자 빠르게 찾기/find-number-fast.py
+++ trail5/숫자 빠르게 찾기/find-number-fast.py
@@ -1,15 +1,14 @@
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
 queries = [int(input()) for _ in range(m)]
-
 for i in queries:
     idx = -1
-    left = 0
     right = n-1
+    left = 0
     while left <= right:
-        mid = (left+right) // 2
+        mid = (left+right) //2
         if arr[mid] == i:
-            idx = mid +1
+            idx = mid+1
             break
         if arr[mid] > i:
             right = mid -1
