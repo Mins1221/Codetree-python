7ab04ca113e64fcc92dff48aea3825b76f5f30d1
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 13:32:42 +0900

    [Passed] 수의 개수 (190ms, 24MB)

--- trail5/수의 개수/number-of-integers.py
+++ trail5/수의 개수/number-of-integers.py
@@ -1,7 +1,6 @@
-from bisect import bisect_right, bisect_left 
+from bisect import bisect_right,bisect_left
 n,m = map(int,input().split())
 arr = list(map(int,input().split()))
 queries = [int(input()) for _ in range(m)]
-
 for i in queries:
     print(bisect_right(arr,i) - bisect_left(arr,i))
