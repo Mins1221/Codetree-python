882af1f3f0640429776b899964cd839fd443dc54
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 16:52:04 +0900

    [Passed] 집합의 원소 (735ms, 26MB)

--- trail6/집합의 원소/elements-of-a-set.py
+++ trail6/집합의 원소/elements-of-a-set.py
@@ -1,18 +1,18 @@
 n,m = map(int,input().split())
-uf = [0]*(n+1)
+uf = [0] *(n+1)
 for i in range(1,n+1):
     uf[i] = i
 def find(x):
-    if uf[x] ==x:
+    if uf[x] == x:
         return x
     uf[x] = find(uf[x])
     return uf[x]
 def union(x,y):
-    X, Y = find(x), find(y)
+    X,Y = find(x),find(y)
     uf[X] = Y
 for _ in range(m):
     q_type,a,b = map(int,input().split())
-    if q_type ==0:
+    if q_type == 0:
         union(a,b)
     else:
         if find(a) == find(b):
