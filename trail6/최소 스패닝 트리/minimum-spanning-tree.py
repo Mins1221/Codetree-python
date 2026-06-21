c292c3e827810fbe13a482b5f90f23bb0c4bbf4d
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 17:30:27 +0900

    [Passed] 최소 스패닝 트리 (544ms, 36MB)

--- trail6/최소 스패닝 트리/minimum-spanning-tree.py
+++ trail6/최소 스패닝 트리/minimum-spanning-tree.py
@@ -1,23 +1,21 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
-
-uf = [0] * (n+1)
+n,m = map(int,input().split())
+edges = [tuple(map(int,input().split())) for _ in range(m)]
+uf = [0] *(n+1)
+for i in range(1,n+1):
+    uf[i] = i
 def find(x):
     if uf[x] == x:
         return x
     uf[x] = find(uf[x])
     return uf[x]
-
-def union (x,y):
-    X,Y = find(x),find(y)
+def union(x,y):
+    X,Y = find(x), find(y)
     uf[X] = Y
-
-for i in range(1,n+1):
-    uf[i] = i
-ans = 0
-edges.sort(key=lambda x: x[2])
-for x,y,cost in edges:
-    if find(x) != find(y):
+ans =0
+edges.sort(key = lambda x : x[2])
+edges
+for a,b,cost in edges:
+    if find(a) != find(b):
         ans += cost
-        union(x,y)
+        union(a,b)
 print(ans)
