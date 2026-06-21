e2751e7771b0e9e9996ddcd245de778c7e949709
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 16:31:50 +0900

    [Wrong Answer] 겹치지 않는 선분위의 두 점 (91ms, 17MB)

--- trail5/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
+++ trail5/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
@@ -1,27 +1,24 @@
 n, m = map(int, input().split())
 segments = [tuple(map(int, input().split())) for _ in range(m)]
-
-INT_MUM = 10**18 +1
-def clac(dist):
-    cnt =0
-    left = -INT_MUM
+MAX_MUM = 10**18 +1
+def calc(dist):
+    left = -MAX_MUM
+    cnt = 0
     for s,e in segments:
         while left + dist <= e:
             cnt +=1
-            left = max(s,left+dist)
+            left = max(left+dist,s)
             if cnt >= n:
                 break
     return cnt >= n
-segments.sort()
 left = 1
-right = INT_MUM
+right = MAX_MUM
 ans = 0
-while left <= right :
+while left <= right:
     mid = (left + right) //2
-    if clac(mid):
+    if calc(mid):
         left = mid +1
-        ans = mid
+        ans =mid
     else:
         right = mid -1
 print(ans)
-
