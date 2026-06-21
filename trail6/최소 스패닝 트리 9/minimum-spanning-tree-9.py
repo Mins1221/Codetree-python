cd8556d72b49f582eeca3310abec448943b7c0cb
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 20:17:51 +0900

    [Passed] 최소 스패닝 트리 9 (544ms, 38MB)

--- trail6/최소 스패닝 트리 9/minimum-spanning-tree-9.py
+++ trail6/최소 스패닝 트리 9/minimum-spanning-tree-9.py
@@ -1,39 +1,26 @@
-import heapq
 import sys
-INT_MAX = sys.maxsize
-
-n, m = map(int, input().split())
+import heapq
+n,m = map(int,input().split())
+input = sys.stdin.readline
 graph = [[] for _ in range(n+1)]
-pq = []
-
-dist = [INT_MAX] * (n+1)
-visited = [False] * (n+1)
-
 for _ in range(m):
-    x,y,z = tuple(map(int,input().split()))
-
-    graph[x].append((y,z))
-    graph[y].append((x,z))
-
-dist[1] = 0
-
+    a,b,w = map(int,input().split())
+    graph[a].append((w,b))
+    graph[b].append((w,a))
+pq =[]
 heapq.heappush(pq,(0,1))
-
+visited= [False]*(n+1)
 ans = 0
+count = 0
 while pq:
-    min_dist, min_index = heapq.heappop(pq)
-
-    if visited[min_index]:
+    w,node = heapq.heappop(pq)
+    if visited[node]:
         continue
-
-    visited[min_index] = True
-    ans += min_dist
-
-    for target_index,target_dist in graph[min_index]:
-        new_dist = target_dist
-        if dist[target_index] > new_dist:
-            dist[target_index] = new_dist
-            heapq.heappush(pq,(new_dist,target_index))
+    visited[node] = True
+    ans +=w
+    count +=1
+    for next_w,next_node in graph[node]:
+        if not visited[next_node]:
+            heapq.heappush(pq,(next_w,next_node))
 print(ans)
-
 
