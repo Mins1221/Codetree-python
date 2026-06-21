2f7c48894fb2cf0363c0593251bcf44667b75f44
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 17:19:43 +0900

    [Passed] 경로의 적합성 판단 2 (355ms, 25MB)

--- trail6/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.py
+++ trail6/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.py
@@ -1,5 +1,5 @@
-n, m, k = map(int, input().split())
-uf = [0] *(n+1)
+n,m,k = map(int,input().split())
+uf = [0] * (n+1)
 for i in range(1,n+1):
     uf[i] = i
 def find(x):
@@ -8,12 +8,12 @@ def find(x):
     uf[x] = find(uf[x])
     return uf[x]
 def union(x,y):
-    X,Y = find(x), find(y)
+    X,Y = find(x) , find(y)
     uf[X] = Y
 for _ in range(m):
     a,b = tuple(map(int,input().split()))
     union(a,b)
-is_pos =True
+is_pos = True
 path = [0] + list(map(int,input().split()))
 for i in range(1,k):
     if find(path[i]) != find(path[i+1]):
