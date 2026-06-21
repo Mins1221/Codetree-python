98a70474a7810f8be35bf4128d414cdcf79f6444
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 21:26:37 +0900

    [Passed] 갈 수 있는 경우의 수 (152ms, 22MB)

--- trail6/갈 수 있는 경우의 수/possible-path-of-travel.py
+++ trail6/갈 수 있는 경우의 수/possible-path-of-travel.py
@@ -1,23 +1,26 @@
+from collections import deque
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-from collections import deque
-MOD = 1000000007
-graph = [[] for _ in range(n + 1)]
-indegree = [0] * (n + 1)
-for x, y in edges:
-    graph[x].append(y)
-    indegree[y] += 1
+graph = [[] for _ in range(n+1)]
 q = deque()
-for i in range(1, n + 1):
-    if indegree[i] == 0:
+indegree = [0] * (n + 1)
+MOD = 1000000007
+for x,y in edges:
+    graph[x].append(y)
+    indegree[y]+=1
+for i in range(1 , n+1):
+    if indegree[i] ==0:
         q.append(i)
 dp = [0] * (n + 1)
-dp[1] = 1
+dp[1] =1
 while q:
-    cur = q.popleft()
-    for nxt in graph[cur]:
-        dp[nxt] = (dp[nxt] + dp[cur]) % MOD
+    cnt = q.popleft()
+    for nxt in graph[cnt]:
+        dp[nxt] = (dp[nxt]+dp[cnt]) % MOD
         indegree[nxt] -= 1
         if indegree[nxt] == 0:
             q.append(nxt)
 print(dp[n])
+
+    
+        
