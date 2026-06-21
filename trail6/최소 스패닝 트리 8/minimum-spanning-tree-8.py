3af2600c158bacf661509fb0e12c61b9a78a1758
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 20:05:57 +0900

    [Passed] 최소 스패닝 트리 8 (94ms, 19MB)

--- trail6/최소 스패닝 트리 8/minimum-spanning-tree-8.py
+++ trail6/최소 스패닝 트리 8/minimum-spanning-tree-8.py
@@ -2,21 +2,21 @@ import sys
 import heapq
 n,m = map(int,input().split())
 graph = [[] for _ in range(n+1)]
+pq= []
 input = sys.stdin.readline
-for _ in range(m):
-    a, b, w = map(int, input().split())
-    graph[a].append((w, b))
-    graph[b].append((w, a))
-visited = [False]*(n+1)
-pq = []
+for i in range(m):
+    a,b,w = map(int,input().split())
+    graph[b].append((w,a))
+    graph[a].append((w,b))
+visited= [False] * (n+1)
 heapq.heappush(pq,(0,1))
-ans = 0
+ans =0
 while pq:
     w,node = heapq.heappop(pq)
     if visited[node]:
         continue
+    ans +=w
     visited[node] = True
-    ans += w
     for next_w, next_node in graph[node]:
         if not visited[next_node]:
             heapq.heappush(pq,(next_w,next_node))
