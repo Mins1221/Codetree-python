16c95c87be0f9225209b2c0797f7f47ce2657db6
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 19:41:15 +0900

    [Passed] 최소 스패닝 트리 8 (82ms, 17MB)

--- trail6/최소 스패닝 트리 8/minimum-spanning-tree-8.py
+++ trail6/최소 스패닝 트리 8/minimum-spanning-tree-8.py
@@ -1,26 +1,31 @@
-import sys
-INT_MAX = sys.maxsize
 n, m = map(int, input().split())
-graph= [[0]*(n+1)  for _ in range(n+1)]
-visited = [False] * (n+1)
-dist = [INT_MAX] * (n+1)
-for _ in range(m):
-    x,y,z = tuple(map(int,input().split()))
-    graph[x][y] = z if graph[x][y] == 0 else min(graph[x][y],z)
-    graph[y][x] = z if graph[y][x] == 0 else min(graph[y][x],z)
-dist[1] = 0
-ans = 0
-for i in range(1,n+1):
-    min_index = -1
-    for j in range(1,n+1):
-        if visited[j] :
-            continue
-        if min_index == -1 or dist[min_index] > dist[j]:
-            min_index = j
-    visited[min_index] = True
-    ans += dist[min_index]
-    for j in range(1,n+1):
-        if graph[min_index][j] == 0:
-            continue
-        dist[j] = min(dist[j] , graph[min_index][j])
-print(ans)
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+uf = list(range(n + 1))
+
+def find(x):
+    while uf[x] != x:
+        uf[x] = uf[uf[x]]
+        x = uf[x]
+
+    return x
+
+def union(x,y):
+    X,Y = find(x), find(y)
+    if X == Y:
+        return False
+    uf[Y] = X
+    return True
+edges.sort(key = lambda x: x[2])
+answer = 0
+cnt = 0
+for u, v, w in edges:
+    if union(u, v):
+        answer += w
+        cnt += 1
+        if cnt == n - 1:
+            break
+print(answer)
+
+
