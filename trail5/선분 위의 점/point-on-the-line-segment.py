90ca8ff12e0176fdcd24b0c7ea330fb7f3829916
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 23:55:45 +0900

    [Passed] 선분 위의 점 (389ms, 36MB)

--- trail5/선분 위의 점/point-on-the-line-segment.py
+++ trail5/선분 위의 점/point-on-the-line-segment.py
@@ -1,7 +1,7 @@
 from bisect import bisect_right, bisect_left
 n,m = map(int,input().split())
-arr = list(int(input().split()))
-line = [int(input().split()) for _ in range(m)]
-
+arr = list(map(int,input().split()))
+line = [list(map(int,input().split())) for _ in range(m)]
+arr.sort()
 for s,e in line:
     print(bisect_right(arr,e) - bisect_left(arr,s))
