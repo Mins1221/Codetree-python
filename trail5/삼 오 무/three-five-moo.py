08e6a31882f73e7bbf3bbe4c5abc206bffb20331
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 15:24:55 +0900

    [Wrong Answer] 삼 오 무 (48ms, 16MB)

--- trail5/삼 오 무/three-five-moo.py
+++ trail5/삼 오 무/three-five-moo.py
@@ -1,16 +1,14 @@
 import sys
 INT_MAX = sys.maxsize
-n = int(input())
 l = 0
 r = INT_MAX
 ans = INT_MAX
-
-while l <= r:
-    mid = (l+r) // 2
-    if mid - mid // 3 - mid // 5 + mid // 15 >=n:
+n = int(input())
+while l<r:
+    mid = (l+r) //2
+    if mid - mid//3 - mid//5 - mid //15 >= n:
         r = mid -1
         ans = min(ans,mid)
     else:
-        l = mid + 1
-
+        l = mid +1
 print(ans)
