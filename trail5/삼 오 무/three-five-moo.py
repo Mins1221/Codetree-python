88be85d2ff4d143ea8d738df8d7657642a87c712
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 15:26:56 +0900

    [Passed] 삼 오 무 (50ms, 16MB)

--- trail5/삼 오 무/three-five-moo.py
+++ trail5/삼 오 무/three-five-moo.py
@@ -4,9 +4,9 @@ l = 0
 r = INT_MAX
 ans = INT_MAX
 n = int(input())
-while l<=r:
-    mid = (l+r) //2
-    if mid - mid//3 - mid//5 + mid //15 >= n:
+while l <= r:
+    mid =(r+l) //2
+    if mid - mid//3 - mid //5 + mid //15 >= n:
         r = mid -1
         ans = min(ans,mid)
     else:
