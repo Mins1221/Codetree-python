dbc68d07d2ad1ceee820c5981140374eabfd4e88
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 23:23:20 +0900

    [Passed] 사이클 없애기 (383ms, 31MB)

--- trail6/사이클 없애기/remove-cycle.py
+++ trail6/사이클 없애기/remove-cycle.py
@@ -1,26 +1,26 @@
 from collections import deque
-n,m1, m2 = map(int,input().split())
-indegree = [0] * (n+1)
+n,m1,m2 = map(int,input().split())
 edges = [[] for _ in range(n+1)]
+indegree = [0] *(n+1)
 q = deque()
-for _ in range(m1):
+for i in range(m1):
     x,y = tuple(map(int,input().split()))
     edges[x].append(y)
     indegree[y] +=1
 for i in range(1,n+1):
     if not indegree[i]:
         q.append(i)
-for _ in range(m2):
+for i in range(m2):
     x,y = tuple(map(int,input().split()))
-ans =[]
+count = 0
 while q:
-    now = q.popleft()
-    ans.append(now)
-    for x in edges[now]:
-        indegree[x] -=1
-        if not indegree[x]:
-            q.append(x)
-if len(ans) == n:
+    x = q.popleft()
+    count +=1
+    for y in edges[x]:
+        indegree[y] -=1
+        if not indegree[y]:
+            q.append(y)
+if count == n:
     print("Yes")
 else:
     print("No")
