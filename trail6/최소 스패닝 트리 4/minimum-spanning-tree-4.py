2808c96bb07c7706c4ecfa3edd2b2d861af6bd02
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 17:45:54 +0900

    [Passed] 최소 스패닝 트리 4 (431ms, 34MB)

--- trail6/최소 스패닝 트리 4/minimum-spanning-tree-4.py
+++ trail6/최소 스패닝 트리 4/minimum-spanning-tree-4.py
@@ -1,35 +1,32 @@
-import sys
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
-# 1~n번 정점의 종류. 실제 입력 형식에 맞춰 이 한 줄만 조정하세요.
-# 예: "0 1 1 0 ..." 형태로 종류가 한 줄에 주어진다고 가정
-color = [None] + input().split()
-
-edges = [tuple(map(int, input().split())) for _ in range(m)]
-
-uf = list(range(n + 1))
+n,m = map(int,input().split())
+uf = [0] *(n+1)
+type = [' '] + list(input().split())
+edges = [tuple(map(int,input().split())) for _ in range(m)]
 def find(x):
-    while uf[x] != x:
-        uf[x] = uf[uf[x]]      # 경로 압축 (반복문 → 재귀 깊이 걱정 X)
-        x = uf[x]
-    return x
-def union(x, y):
-    X, Y = find(x), find(y)
-    if X == Y:
-        return False
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+def union(x,y):
+    X,Y = find(x),find(y)
     uf[X] = Y
-    return True
-
-ans = 0
-cnt = 0                         # 실제로 사용한 간선 수
-edges.sort(key=lambda e: e[2])
-for a, b, cost in edges:
-    if color[a] == color[b]:    # 같은 종류를 잇는 간선은 사용 불가
+for i in range(1,n+1):
+    uf[i] = i
+edges.sort(key = lambda x:x[2])
+ans =0
+for x,y,cost in edges:
+    if type[x]==type[y]:
         continue
-    if union(a, b):
+    if find(x) != find(y):
+        union(x,y)
         ans += cost
-        cnt += 1
-
-print(ans if cnt == n - 1 else -1)   # n-1개 못 채우면 트리 불가능
+is_all_connected= True
+for i in range(2,n+1):
+    x = find(1)
+    y = find(i)
+    if x != y:
+        is_all_connected = False
+if is_all_connected:
+    print(ans)
+else:
+    print(-1)
