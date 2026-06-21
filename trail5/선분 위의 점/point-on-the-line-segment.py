0295ef8254c2bd4eb605cc4d3862338fe7d42c3c
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 23:53:38 +0900

    [Runtime Error] 선분 위의 점 (82ms, 19MB)

--- trail5/선분 위의 점/point-on-the-line-segment.py
+++ trail5/선분 위의 점/point-on-the-line-segment.py
@@ -1,8 +1,7 @@
-from bisect import bisect_right,bisect_left
+from bisect import bisect_right, bisect_left
 n,m = map(int,input().split())
-arr = list(map(int,input().split()))
-lines = [list(map(int,input().split())) for _ in range(m)]
+arr = list(int(input().split()))
+line = [int(input().split()) for _ in range(m)]
 
-arr.sort()
-for s,e in lines:
+for s,e in line:
     print(bisect_right(arr,e) - bisect_left(arr,s))
