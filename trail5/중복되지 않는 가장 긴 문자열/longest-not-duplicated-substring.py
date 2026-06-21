c7d537392ff2a78d28cd9ed3c7f5738212dea5d6
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-21 23:28:35 +0900

    [Passed] 중복되지 않는 가장 긴 문자열 (61ms, 17MB)

--- trail5/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.py
+++ trail5/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.py
@@ -1,12 +1,13 @@
 word = input()
-n = len(word)
+n =len(word)
 j = -1
-ans =0
 count = {}
+ans = 0
 for i in range(n):
-    while j+1 < n and count.get(word[j+1],0) != 1:
+    while j +1 < n and count.get(word[j+1],0) != 1:
         count[word[j+1]] = count.get(word[j+1],0) + 1
         j +=1
     ans = max(ans,j-i+1)
     count[word[i]] -= 1
 print(ans)
+
