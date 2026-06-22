6f59c9bc166d383ca33f7f1856daa89909088f29
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-22 13:35:35 +0900

    [Passed] 각 정점까지의 최단 경로 3 (80ms, 17MB)

--- trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
+++ trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -1,24 +1,25 @@
 import heapq
-n, m = map(int, input().split())
+import sys
+n,m = map(int,input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-INT_MAX = float('inf')
-graph = [[] for _ in range(n+1)]
-for u, v, w in edges:
-    graph[u].append((v,w)) 
-pq = []
-visited = [False]*(n+1)
+INT_MAX = sys.maxsize
 dist = [INT_MAX]*(n+1)
+visited= [False]*(n+1)
+graph = [[] for _ in range(n+1)]
+pq = []
+for u,v,w in edges:
+    graph[u].append((v,w))
 dist[1] = 0
-heapq.heappush(pq, (0, 1))
+heapq.heappush(pq,(0,1))
 while pq:
     min_dist, index = heapq.heappop(pq)
     if not visited[index]:
         visited[index] = True
-        for v, w in graph[index]:
+        for v,w in graph[index]:
             if min_dist + w < dist[v]:
                 dist[v] = min_dist + w
-                heapq.heappush(pq, (dist[v], v))
-for i in range(2, n+1):
+                heapq.heappush(pq,(dist[v],v))
+for i in range(2,n+1):
     if dist[i] == INT_MAX:
         print(-1)
     else:
