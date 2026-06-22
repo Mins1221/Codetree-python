e1359657a57a7e10549a38d13c9f7a0db79a6c4e
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-22 13:18:55 +0900

    [Passed] 최소 스패닝 트리 4 (516ms, 36MB)

--- trail6/최소 스패닝 트리 4/minimum-spanning-tree-4.py
+++ trail6/최소 스패닝 트리 4/minimum-spanning-tree-4.py
@@ -12,8 +12,8 @@ def find(x):
 def union(x,y):
     X,Y = find(x),find(y)
     uf[X] = Y
-ans =0
-edges.sort(key = lambda x:x[2])
+ans = 0
+edges.sort(key=lambda x:x[2])
 for x,y,cost in edges:
     if type[x] == type[y]:
         continue
@@ -21,10 +21,10 @@ for x,y,cost in edges:
         ans += cost
         union(x,y)
 is_all_connected = True
-for i in range(2,n+1):
+for i in range(1,n+1):
     x = find(1)
     y = find(i)
-    if x!=y:
+    if x != y:
         is_all_connected = False
 if is_all_connected :
     print(ans)
