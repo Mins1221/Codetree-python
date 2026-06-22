9ac758d1b4d26cfb8d5522a3b9c9d3ccf24e4241
HWANG MINSEUNG <127467371+Mins1221@users.noreply.github.com>
2026-06-22 13:27:40 +0900

    [Passed] 각 정점까지의 최단 경로 3 (89ms, 17MB)

--- trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
+++ trail5/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -1,28 +1,27 @@
 import sys
-import heapq
-n, m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+INT_MAX = sys.maxsize
+n, m = tuple(map(int, input().split()))
+graph = [[0] * (n + 1)for _ in range(n + 1)]
+visited = [False] * (n + 1)
+dist = [INT_MAX] * (n + 1) 
 for _ in range(m):
-    start, end, weight = map(int, input().split())
-    graph[start].append((end, weight))
-INF = sys.maxsize
-dist = [INF] * (n+1)
-def dijkstra(start):
-    pq = []
-    dist[start] = 0
-    heapq.heappush(pq, (0, start))
-    while pq:
-        cur_dist, cur_node = heapq.heappop(pq)
-        if cur_dist > dist[cur_node]:
+    x, y, z = tuple(map(int, input().split()))
+    graph[x][y] = z
+dist[1] = 0
+for i in range(1, n + 1):
+    min_index = -1
+    for j in range(1, n + 1):
+        if visited[j]:
             continue
-        for next_node, weight in graph[cur_node]:
-            new_dist = cur_dist + weight
-            if new_dist < dist[next_node]:
-                dist[next_node] = new_dist
-                heapq.heappush(pq, (new_dist, next_node))
-dijkstra(1)
-for i in range(2, n+1):
-    if dist[i] == INF:
+        if min_index == -1 or dist[min_index] > dist[j]:
+            min_index = j
+    visited[min_index] = True
+    for j in range(1, n + 1):
+        if graph[min_index][j] == 0:
+            continue
+        dist[j] = min(dist[j], dist[min_index] + graph[min_index][j])
+for i in range(2, n + 1):
+    if dist[i] == INT_MAX:
         print(-1)
     else:
         print(dist[i])
